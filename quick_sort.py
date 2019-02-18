a0d64c6523cfc228f4ef4cf06369620980e80146
satojkovic <satojkovic@gmail.com>
2019-02-18 21:19:43 +0900

    quick sort in place

--- quick_sort.py
+++ quick_sort.py
@@ -17,7 +17,6 @@ def quick_sort(data):
     Returns:
         list: a sorted list
     """
-
     if len(data) < 2:
         return data
 
@@ -35,7 +34,42 @@ def quick_sort(data):
     return left + [pivot_val] + right
 
 
+def quick_sort_in_place(data):
+    """quick sort in place partition
+
+    Args:
+        data (list): an input list
+
+    Returns:
+        list: a sorted list
+    """
+    return _quick_sort(data, 0, len(data) - 1)
+
+
+def _quick_sort(data, l, r):
+    if l >= r:
+        return
+    mid = partition(data, l, r)
+    _quick_sort(data, l, mid - 1)
+    _quick_sort(data, mid + 1, r)
+    return data
+
+
+def partition(data, l, r):
+    pivot = data[l]
+    i = l + 1
+    for j in range(l + 1, r + 1):
+        if pivot > data[j]:
+            data[i], data[j] = data[j], data[i]
+            i += 1
+    data[i - 1], data[l] = data[l], data[i - 1]
+    return i - 1
+
+
 if __name__ == "__main__":
     data = [30, 2, 10, 100, 24, 7, 79]
     print('org:', data)
     print('sorted:', quick_sort(data))
+
+    data = [30, 2, 10, 100, 24, 7, 79]
+    print('sorted(in place):', quick_sort_in_place(data))
