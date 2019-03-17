c5bc0be6c6ce5cf8b49e6a8bd991c83ca8535ee8
satojkovic <satojkovic@gmail.com>
2019-03-18 08:16:34 +0900

    Insert at tail of the linked list

--- linked_list.py
+++ linked_list.py
@@ -16,6 +16,8 @@ class LinkedList:
 
     def __init__(self):
         self.head = None
+        self.tail = None
+        self.size = 0
 
     def is_empty(self):
         # check whether head is None
@@ -23,3 +25,43 @@ class LinkedList:
             return True
         else:
             return False
+
+    def clear(self):
+        # clear the linked list, O(n)
+        trav = self.head
+        while not trav:
+            next_elem = trav.next_elem
+            trav.next_elem = None
+            trav.data = None
+            trav = next_elem
+
+    def add(self, elem):
+        self.add_last(elem)
+
+    def add_last(self, elem):
+        # Add a node to the tail of the linked list, O(1)
+        if self.is_empty():
+            node = ListElement(elem)
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next_elem = ListElement(elem)
+            self.tail = self.tail.next_elem
+        self.size += 1
+
+    def print_list(self):
+        if self.is_empty():
+            print('List is empty.')
+            return
+        temp = self.head
+        print('List:', end=' ')
+
+        while temp:
+            print(temp.data, end='->')
+            temp = temp.next_elem
+        print('None')
+
+if __name__ == "__main__":
+    l = LinkedList()
+    [l.add(i) for i in range(10)]
+    l.print_list()
