e6ed2f2c46fe1c8e88bedb38df188c431a8910fd
satojkovic <satojkovic@gmail.com>
2019-04-19 22:33:42 +0900

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -4,10 +4,6 @@ using namespace std;
 
 class Stack
 {
-    public:
-        static const int SUCCESS = 0;
-        static const int ERR_STACK_EMPTY = 1;
-
     private:
         int* arr;
         int capacity;
@@ -17,8 +13,10 @@ class Stack
     public:
         Stack(int size);
         bool isEmpty();
-        int peek(int& value);
+        int peek();
         int getSize();
+        bool push(int value);
+        int pop();
 };
 
 Stack::Stack(int size) {
@@ -35,13 +33,12 @@ bool Stack::isEmpty() {
     }
 }
 
-int Stack::peek(int& value) {
+int Stack::peek() {
     if (numElements == 0) {
-        cout << "Stack is empty." << endl;
-        return -1;
+        std::cout << "Stack is empty." << endl;
+        exit(EXIT_FAILURE);
     } else {
-        value = top;
-        return SUCCESS;
+        return top;
     }
 }
 
@@ -49,15 +46,41 @@ int Stack::getSize() {
     return numElements;
 }
 
+bool Stack::push(int value) {
+    if (numElements < capacity) {
+        arr[numElements] = value;
+        numElements++;
+        top = value;
+        return true;
+    } else {
+        std::cout << "Stack is full" << endl;
+        return false;
+    }
+}
+
+int Stack::pop() {
+    if(numElements == 0) {
+        std::cout << "Stack is empty" << endl;
+        exit(EXIT_FAILURE);
+    } else {
+        numElements--;
+        top = arr[numElements - 1];
+        return arr[numElements];
+    }
+}
+
+
 int main() {
     Stack s(10);
-    int top_value = 0;
-    int ret = s.peek(top_value);
-    if (ret == Stack::SUCCESS) {
-        cout << "top() : " << top_value << endl;
-    }
-
-    cout << "getSize() : " << s.getSize() << endl;
+    s.push(5);
+    s.push(10);
+    s.push(15);
+    std::cout << "getSize() : " << s.getSize() << endl;
+    std::cout << "top : " << s.peek() << endl;
+    std::cout << "pop : " << s.pop() << endl;
+    std::cout << "pop : " << s.pop() << endl;
+    std::cout << "pop : " << s.pop() << endl;
+    std::cout << "getSize() : " << s.getSize() << endl;
 
     return 0;
 }
