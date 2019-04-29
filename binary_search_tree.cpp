980d92cedb24fa18640bdc7cf3598c9d5b9e3717
satojkovic <satojkovic@gmail.com>
2019-04-29 20:37:31 +0900

    addNode

--- binary_search_tree.cpp
+++ binary_search_tree.cpp
@@ -21,19 +21,80 @@ class TreeNode {
 
 class BinarySearchTree {
     TreeNode* root;
+    int num_nodes;
 
     public:
         BinarySearchTree(int elem) {
             root = new TreeNode(elem);
+            num_nodes = 1;
         }
 
         TreeNode* getRoot() {
             return root;
+        }
+
+        bool contains(int elem) {
+            return contains_r(root, elem);
+        }
+
+        bool contains_r(TreeNode* node, int elem) {
+            if (node == nullptr) {
+                return false;
+            }
+
+            if (elem < node->data) {
+                return contains_r(node->left, elem);
+            } else if (elem > node->data)
+            {
+                return contains_r(node->right, elem);
+            } else {
+                return true;
+            }
+        }
+
+        bool addNode(int elem) {
+            if (contains(elem)) {
+                return false;
+            } else {
+                root = addNode_r(root, elem);
+                num_nodes++;
+                return true;
+            }
+        }
+
+        TreeNode* addNode_r(TreeNode* node, int elem) {
+            if (node == nullptr) {
+                node = new TreeNode(elem);
+            } else {
+                if (elem < node->data) {
+                    node->left = addNode_r(node->left, elem);
+                } else {
+                    node->right = addNode_r(node->right, elem);
+                }
+            }
+            return node;
+        }
+
+        void inOrderPrint(TreeNode* node) {
+            if (node != nullptr) {
+                inOrderPrint(node->left);
+                std::cout << node->data << std::endl;
+                inOrderPrint(node->right);
+            }
         }
 };
 
 int main() {
     BinarySearchTree bst(3);
     std::cout << "root value : " << bst.getRoot()->data << std::endl;
+
+    bst.addNode(2);
+    bst.addNode(5);
+    bst.addNode(-1);
+    bst.addNode(0);
+    bst.addNode(10);
+    bst.addNode(0);
+
+    bst.inOrderPrint(bst.getRoot());
     return 0;
 }
