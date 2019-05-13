4e7d36a66d7a1708b0a8b20f6bc25c76c094b3f5
satojkovic <satojkovic@gmail.com>
2019-05-13 21:22:23 +0900

    Insertion (HashTable)

--- hash_table.cpp
+++ hash_table.cpp
@@ -7,7 +7,11 @@ class HashEntry {
 
     HashEntry* next;
 
-    HashEntry() {};
+    HashEntry() {
+        key = "";
+        value = -1;
+        next = nullptr;
+    }
     HashEntry(std::string key, int value) {
         this->key = key;
         this->value = value;
@@ -17,13 +21,16 @@ class HashEntry {
 
 class HashTable {
     public:
-    HashEntry* bucket;
+    HashEntry** bucket;
     int slots;
     int size;
 
-    HashTable() {
-        bucket = new HashEntry();
-        slots = 10;
+    HashTable(int s) {
+        bucket = new HashEntry*[s];
+        for (int i = 0; i < s; i++) {
+            bucket[i] = nullptr;
+        }
+        slots = s;
         size = 0;
     }
 
@@ -45,10 +52,49 @@ class HashTable {
         }
         return Key % slots;
     }
+
+    void insert(std::string key, int value) {
+        int hash_index = getIndex(key);
+        if (bucket[hash_index] == nullptr) {
+            bucket[hash_index] = new HashEntry(key, value);
+            size++;
+        } else {
+            // chaining strategy
+            HashEntry* temp = bucket[hash_index];
+            while (temp->next != nullptr) {
+                temp = temp->next;
+            }
+            temp->next = new HashEntry(key, value);
+            size++;
+        }
+    }
+
+    void display() {
+        HashEntry* temp;
+        std::cout << "HashTable size : " << size << std::endl;
+        for (int i = 0; i < slots; i++) {
+            if (bucket[i] != nullptr) {
+                std::cout << "HashTable index : " << i << " ";
+                temp = bucket[i];
+                while (temp != nullptr) {
+                    std::cout << "(key = " << temp->key << ", value = " << temp->value << " )";
+                    temp = temp->next;
+                }
+            }
+            std::cout << std::endl;
+        }
+    }
 };
 
 int main() {
-    HashTable ht;
-    std::cout << "HashTable is initilized => " << ht.isEmpty() << std::endl;
+    HashTable ht(3);
+    ht.insert("London",2);
+    ht.insert("London",10);
+    ht.insert("New York",15);
+    ht.insert("Tokyo",7);
+    ht.insert("Bangkok",2);
+    ht.insert("Beijing",6);
+    ht.insert("Islamabad",9);
+    ht.display();
     return 0;
 }
