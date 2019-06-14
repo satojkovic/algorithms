a86911c07fc96488a19d33bcff1186617bbb164c
satojkovic <satojkovic@gmail.com>
2019-06-14 09:50:04 +0900

    Implement poll method

--- priority_queue.py
+++ priority_queue.py
@@ -59,6 +59,19 @@ class PQueue:
                 self.heap = self.heapify(self.heap, last_pidx, last_idx)
                 last_pidx = (last_pidx - 2) // 2 if last_pidx % 2 == 0 else (last_pidx - 1) // 2
 
+    def poll(self):
+        if self.is_empty():
+            return None
+
+        self.heap[0], self.heap[-1] = self.heap[-1], self.heap[0]
+        ret = self.heap.pop()
+        self.size -= 1
+
+        last_idx = self.size - 1
+        last_pidx = (last_idx - 2) // 2 if last_idx % 2 == 0 else (last_idx - 1) // 2
+        for p in range(last_pidx, -1, -1):
+            self.heap = self.heapify(self.heap, p, last_idx)
+
     def print_pqueue(self):
         if self.is_empty():
             print('PQueue is empty.')
@@ -79,4 +92,7 @@ if __name__ == "__main__":
     pq = PQueue([5, 6, 8, 7, 12, 14, 19, 13, 12, 11])
     pq.add(1)
     print(pq.heap)
+    pq.print_pqueue()
+
+    pq.poll()
     pq.print_pqueue()
