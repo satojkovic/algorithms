0c7130e235bb9e31f832e7618665aba417090ffb
satojkovic <satojkovic@gmail.com>
2019-06-21 21:31:14 +0900

    Insert method

--- hash_table.py
+++ hash_table.py
@@ -13,14 +13,77 @@ class HashTable:
         self.load_factor = load_factor
         self.size = 0
         self.table = self.capacity * [None]
+        self.threshold = int(self.capacity * self.load_factor)
 
     def is_empty(self):
         return self.size == 0
 
+    def insert(self, key, value):
+        if key is None:
+            return False
+        bucket_index = self._get_index(key)
+        return self._insert_entry(bucket_index, key, value)
+
+    def print_table(self):
+        print('(capacity, size, threshold) = ({}, {}, {}):'.format(self.capacity, self.size, self.threshold))
+        for i, bucket in enumerate(self.table):
+            print('[bucket {}]'.format(i), end=' ')
+            head = bucket
+            while head:
+                print('{} => {}'.format(head.key, head.value), end=' ')
+                head = head.next
+            print()
+
     def _get_index(self, key):
         return (hash(key) & 0x7fffffff) % self.capacity
 
-    def bucket_seek(self, bucket_index, key):
+    def _add_last(self, bucket_index, key, value):
+        bucket = self.table[bucket_index]
+        if not bucket:
+            self.table[bucket_index] = HashEntry(key, value)
+            self.size += 1
+            return
+
+        head = bucket
+        while head.next:
+            head = head.next
+        head.next = HashEntry(key, value)
+        self.size += 1
+
+    def _insert_entry(self, bucket_index, key, value):
+        # exist_entry is None if same value doesn't exist in the bucket
+        exist_entry = self._bucket_seek(bucket_index, key)
+        if not exist_entry:
+            # Append HashEntry
+            self._add_last(bucket_index, key, value)
+            if self.size > self.threshold:
+                self._resize_table()
+        else:
+            # Update value
+            exist_entry.value = value
+        return
+
+    def _resize_table(self):
+        self.capacity *= 2
+        self.threshold = int(self.capacity * self.load_factor)
+        new_table = self.capacity * [None]
+
+        for bucket in self.table:
+            if bucket:
+                head = bucket
+                while head:
+                    bucket_index = self._get_index(head.key)
+                    if new_table[bucket_index]:
+                        new_table_head = new_table[bucket_index]
+                        while new_table_head.next:
+                            new_table_head = new_table_head.next
+                        new_table_head.next = HashEntry(head.key, head.value)
+                    else:
+                        new_table[bucket_index] = HashEntry(head.key, head.value)
+                    head = head.next
+        self.table = new_table
+
+    def _bucket_seek(self, bucket_index, key):
         if key is None:
             return None
 
@@ -33,3 +96,17 @@ class HashTable:
                 return head
             head = head.next
         return None
+
+if __name__ == "__main__":
+    ht = HashTable()
+    print('Init:')
+    ht.print_table()
+    print('Insert:')
+    ht.insert('william', 21)
+    ht.print_table()
+    print('Insert:')
+    ht.insert('bob', 40)
+    ht.print_table()
+    print('Insert:')
+    ht.insert('ken', 11)
+    ht.print_table()
