cd3eaab4afcbd8d54a8589b73251b542b6e27a82
satojkovic <satojkovic@gmail.com>
2019-06-29 09:10:23 +0900

    test_add_first

--- test_linked_list.py
+++ test_linked_list.py
@@ -8,5 +8,14 @@ class TestLinkedList(unittest.TestCase):
     def test_empty(self):
         self.assertEqual(self.l.is_empty(), True)
 
+    def test_add_first(self):
+        self.l.add_first(1)
+        self.assertEqual(self.l.size, 1)
+        self.assertEqual(self.l.peek_first(), 1)
+
+        self.l.add_first(2)
+        self.assertEqual(self.l.size, 2)
+        self.assertEqual(self.l.peek_first(), 2)
+
 if __name__ == "__main__":
     unittest.main()
