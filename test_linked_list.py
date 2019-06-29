c7088798950999dc7c7dddcb3a85892484265cae
satojkovic <satojkovic@gmail.com>
2019-06-29 19:47:57 +0900

    Update

--- test_linked_list.py
+++ test_linked_list.py
@@ -1,21 +1,33 @@
 import linked_list
 import unittest
+from nose.tools import ok_, eq_
 
 class TestLinkedList(unittest.TestCase):
     def setUp(self):
         self.l = linked_list.LinkedList()
 
     def test_empty(self):
-        self.assertEqual(self.l.is_empty(), True)
+        ok_(self.l.is_empty())
+        self.l.add(1)
+        ok_(not self.l.is_empty())
+
+    def test_size(self):
+        eq_(self.l.size, 0)
+        self.l.add(1)
+        eq_(self.l.size, 1)
 
     def test_add_first(self):
         self.l.add_first(1)
-        self.assertEqual(self.l.size, 1)
-        self.assertEqual(self.l.peek_first(), 1)
+        eq_(self.l.peek_first(), 1)
 
         self.l.add_first(2)
-        self.assertEqual(self.l.size, 2)
-        self.assertEqual(self.l.peek_first(), 2)
+        eq_(self.l.peek_first(), 2)
+
+    def test_add_last(self):
+        self.l.add_last(3)
+        eq_(self.l.peek_last(), 3)
+        self.l.add_last(4)
+        eq_(self.l.peek_last(), 4)
 
 if __name__ == "__main__":
     unittest.main()
