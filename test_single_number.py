fb35b9f2a73db32ac6a1bcc4fa5709971a402812
satojkovic <satojkovic@gmail.com>
2019-08-04 19:27:49 +0900

    Add test case for single_number.py

--- test_single_number.py
+++ test_single_number.py
@@ -6,10 +6,12 @@ class TestSingleNumber(unittest.TestCase):
     def test_single_number1(self):
         eq_(single_number1([2, 2, 1]), 1)
         eq_(single_number1([4, 1, 2, 1, 2]), 4)
+        eq_(single_number1([1]), 1)
 
     def test_single_number2(self):
         eq_(single_number2([2, 2, 1]), 1)
         eq_(single_number2([4, 1, 2, 1, 2]), 4)
+        eq_(single_number2([1]), 1)
 
 if __name__ == "__main__":
     unittest.main()
