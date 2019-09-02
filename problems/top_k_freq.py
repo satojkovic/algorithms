91b6ad4e4fa7f53b0565f23e6feb6a49ade5c393
satojkovic <satojkovic@gmail.com>
2019-09-02 20:54:35 +0900

    Update top-k freq

--- problems/top_k_freq.py
+++ problems/top_k_freq.py
@@ -1,6 +1,15 @@
+# Time complexity: O(nlog(n))
+#  iteration of the list is O(n)
+#  the built-in sorting algorithm of python uses Timsort, which runs in O(nlog(n)).
+#  get sliced list is O(k). (k is less than n)
+#  Therefore the overall time complexity is O(nlog(n))
+#
+# Space complexity: O(n)
+#  depends on the length of nums, which is O(n).
+#
 # Algorithm:
 #  Two step algorithm. 
-#  1. Count the frequency and stores the freq to a dictionary.
+#  1. Count the frequency and stores the freq to the dictionary.
 #  2. Sort the dictionary by value
 #  Finally return the top k element from the sorted list.
 def top_k_freq1(nums, k):
