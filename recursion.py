89244ab930fb0dd1e3fbb5bdfed538d465ad8321
satojkovic <satojkovic@gmail.com>
2019-10-09 22:40:23 +0900

    Add two functions to recursion.py
    reverse_str2: list input
    reverse_str3: reverse string in place

--- recursion.py
+++ recursion.py
@@ -8,3 +8,15 @@ def reverse_str(s):
         return s
     rstr = reverse_str(s[1:])
     return ''.join([rstr, s[0]])
+
+def reverse_str2(s):
+    if len(s) == 0:
+        return []
+    rstr = reverse_str2(s[1:])
+    return rstr + [s[0]]
+
+def reverse_str3(s, head, tail):
+    if head >= tail:
+        return
+    s[head], s[tail] = s[tail], s[head]
+    return reverse_str3(s, head + 1, tail - 1)
--- test_recursion.py
+++ test_recursion.py
@@ -14,5 +14,28 @@ class TestRecursion(unittest.TestCase):
         eq_(reverse_str(''), '')
         eq_(reverse_str('AB'), 'BA')
 
+    def test_reverse_str2(self):
+        eq_(reverse_str2(['a', 'b', 'c', 'd', 'e']), ['e', 'd', 'c', 'b', 'a'])
+        eq_(reverse_str2(['A']), ['A'])
+        eq_(reverse_str2([]), [])
+        eq_(reverse_str2(['A', 'B']), ['B', 'A'])
+
+    def test_reverse_str3(self):
+        s = ['a', 'b', 'c', 'd', 'e']
+        reverse_str3(s, 0, len(s) - 1)
+        eq_(s, ['e', 'd', 'c', 'b', 'a'])
+
+        s = ['A']
+        reverse_str3(s, 0, len(s) - 1)
+        eq_(s, ['A'])
+
+        s = []
+        reverse_str3(s, 0, len(s) - 1)
+        eq_(s, [])
+
+        s = ['A', 'B']
+        reverse_str3(s, 0, len(s) - 1)
+        eq_(s, ['B', 'A'])
+
 if __name__ == "__main__":
     unittest.main()
