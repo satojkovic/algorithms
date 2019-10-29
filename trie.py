1558ef8140bd572b2a5a02ede613613c5b59fa4c
satojkovic <satojkovic@gmail.com>
2019-10-29 22:09:46 +0900

    Add word_finished to TrieNode class

--- trie.py
+++ trie.py
@@ -2,13 +2,16 @@ class TrieNode:
     def __init__(self, data):
         self.data = data
         self.children = {}
+        self.word_finished = False
 
 def insert(root, s):
     if len(s) == 1:
-        root.children[s] = TrieNode(root.data + s)
+        if not s in root.children:
+            root.children[s] = TrieNode(root.data + s)
+        root.children[s].word_finished = True
         return root
 
-    if not root.children.get(s[0]):
+    if not s[0] in root.children:
         root.children[s[0]] = TrieNode(root.data + s[0])
     child = insert(root.children[s[0]], s[1:])
     root.children[s[0]] = child
@@ -16,7 +19,7 @@ def insert(root, s):
 
 def search(root, s):
     if len(s) == 0:
-        return True
+        return True if root.word_finished else False
     if not s[0] in root.children:
         return False
     return search(root.children[s[0]], s[1:])
