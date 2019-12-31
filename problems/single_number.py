e2cf897fe244b6015862470b1a818c7d300dde0c
satojkovic <satojkovic@gmail.com>
2019-12-31 14:06:16 +0900

    Update single_number

--- problems/single_number.py
+++ problems/single_number.py
@@ -1,17 +1,31 @@
-# Time complexity: O(n) for loop, hash table operation(get) is O(1) at average case
-# Space complexity: O(n)
+# Time complexity: `for` loop is O(n), hash table operation(pop) is O(1)
+# Space complexity: The space required by `hash table` is equal to the number of elements in `nums`
 def single_number1(nums):
     uniq = {}
     for num in nums: # O(n)
-        uniq[num] = uniq.get(num, 0) + 1 # O(1)
-    for k, v in uniq.items(): # O(n)
-        if v == 1:
-            return k
+        try:
+            uniq.pop(num)
+        except:
+            uniq[num] = 1
+    return uniq.popitem()[0]
 
-# Time complexity: O(n) for loop
+# If we take XOR of two same bits, it will return 0
+# If we take XOR of zero and some bit, it will return that bit
+# So we can XOR all bits together to find unique number.
+#
+# Time complexity: `for` loop is O(n)
 # Space complexity: O(1)
 def single_number2(nums):
     c = 0
     for num in nums:
         c ^= num
     return c
+
+# Time complexity: list.sort is O(nlogn), and `for` loop is O(n)
+# Space complexity: O(1)
+def single_number3(nums):
+    nums.sort()
+    for i in range(0, len(nums), 2):
+        if i == len(nums) - 1 or nums[i] != nums[i + 1]:
+            return nums[i]
+    return None
--- problems/test_single_number.py
+++ problems/test_single_number.py
@@ -1,5 +1,5 @@
 import unittest
-from single_number import single_number1, single_number2
+from single_number import *
 from nose.tools import eq_
 
 class TestSingleNumber(unittest.TestCase):
@@ -12,6 +12,11 @@ class TestSingleNumber(unittest.TestCase):
         eq_(single_number2([2, 2, 1]), 1)
         eq_(single_number2([4, 1, 2, 1, 2]), 4)
         eq_(single_number2([1]), 1)
+
+    def test_single_number3(self):
+        eq_(single_number3([2, 2, 1]), 1)
+        eq_(single_number3([4, 1, 2, 1, 2]), 4)
+        eq_(single_number3([1]), 1)
 
 if __name__ == "__main__":
     unittest.main()
