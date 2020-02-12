dc384ce38cb25e4b17b1e7aca772a4b754d6f183
satojkovic <satojkovic@gmail.com>
2020-02-12 22:17:13 +0900

    Update

--- searching/binary_search.go
+++ searching/binary_search.go
@@ -22,8 +22,24 @@ func binarySearch(data []int, target int) bool {
 	return true
 }
 
+func binarySearchR(data []int, target int, low int, high int) bool {
+	if low > high {
+		return false
+	}
+	mid := (low + high) / 2
+	if data[mid] == target {
+		return true
+	} else if data[mid] > target {
+		return binarySearchR(data, target, low, mid-1)
+	} else {
+		return binarySearchR(data, target, mid+1, high)
+	}
+}
+
 func main() {
 	data := []int{1, 10, 11, 25, 33, 52, 74, 100}
 	fmt.Println(binarySearch(data, 11))
 	fmt.Println(binarySearch(data, 123))
+	fmt.Println(binarySearchR(data, 11, 0, len(data)-1))
+	fmt.Println(binarySearchR(data, 123, 0, len(data)-1))
 }
