d96162883b5af48c2b71056001b9bb04464fb7fd
satojkovic <satojkovic@gmail.com>
2020-02-21 21:33:55 +0900

    Walls and gates by BFS

--- problems/walls_and_gates.py
+++ problems/walls_and_gates.py
@@ -4,9 +4,9 @@ def walls_and_gates(rooms):
     for row in range(len(rooms)):
         for col in range(len(rooms[0])):
             if rooms[row][col] != 0 and rooms[row][col] != -1:
-                rooms[row][col] = bfs(rooms, row, col)
+                rooms[row][col] = bf(rooms, row, col)
 
-def bfs(rooms, row, col):
+def bf(rooms, row, col):
     m = len(rooms)
     n = len(rooms[0])
     dist = [n * [0] for _ in range(m)]
@@ -27,6 +27,31 @@ def bfs(rooms, row, col):
             q.append((r, c))
     return rooms[row][col]
 
+def walls_and_gates_rev(rooms):
+    if len(rooms) == 0:
+        return
+    m = len(rooms)
+    n = len(rooms[0])
+    q = []
+    for row in range(m):
+        for col in range(n):
+            if rooms[row][col] == 0:
+                q.append((row, col))
+    INF = 2147483647
+    # Use BFS
+    # mark all rooms distance d from gate
+    # then mark all rooms distance d + 1 from gate
+    # and so on...
+    while q:
+        row, col = q.pop(0)
+        for step_r, step_c in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            r = row + step_r
+            c = col + step_c
+            if r < 0 or r >= m or c < 0 or c >= n or rooms[r][c] != INF:
+                continue
+            rooms[r][c] = rooms[row][col] + 1
+            q.append((r, c))
+
 if __name__ == "__main__":
     INF = 2147483647
     rooms = [
@@ -35,5 +60,5 @@ if __name__ == "__main__":
         [INF, -1, INF, -1],
         [0, -1, INF, INF]
     ]
-    walls_and_gates(rooms)
+    walls_and_gates_rev(rooms)
     print(rooms)
