f0bfe5b3a61c2c5f306d64dac29763248dd63479
satojkovic <satojkovic@gmail.com>
2020-02-29 21:41:15 +0900

    Recursive pascal triangle

--- problems/pascal_triangle.py
+++ problems/pascal_triangle.py
@@ -17,3 +17,16 @@ def pascal_triangle1(num_rows):
                 elems.append(ret[i-1][j] + ret[i-1][j-1])
         ret.append(elems)
     return ret
+
+def pascal_triangle2(row_idx):
+    if row_idx == 0:
+        return [1]
+    prev_res = pascal_triangle2(row_idx - 1)
+    length = row_idx + 1
+    res = length * [0]
+    for i in range(length):
+        if i == 0 or i == length - 1:
+            res[i] = 1
+        else:
+            res[i] = prev_res[i - 1] + prev_res[i]
+    return res
