05928cdeea8d8819855123b2b8414f11494215bc
satojkovic <satojkovic@gmail.com>
2020-03-02 22:04:31 +0900

    Iterative binary search

--- binary_search.py
+++ binary_search.py
@@ -66,6 +66,18 @@ def _binary_search(data, target, left, right):
     else:
         return _binary_search(data, target, mid + 1, right)
 
+def binary_search_iter(data, target):
+    lo = 0
+    hi = len(data) - 1
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        if data[mid] == target:
+            return mid
+        elif target < data[mid]:
+            hi = mid - 1
+        else:
+            lo = mid + 1
+    return -1
 
 if __name__ == "__main__":
     data = [1, 3, 4, 13, 40, 193]
