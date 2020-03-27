35773999ac4c01d5d43fe73601e24c60e55aa76c
satojkovic <satojkovic@gmail.com>
2020-03-27 22:20:40 +0900

    Number of islands

---
+++ problems/num_islands.py
@@ -0,0 +1,17 @@
+def numIslands(grid):
+    num_islands = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == '1':
+                dfs(grid, row, col)
+                num_islands += 1
+    return num_islands
+
+def dfs(grid, row, col):
+    if row < 0 or col < 0 or row >= len(grid) or col >= len(grid[0]) or grid[row][col] != '1':
+        return
+    grid[row][col] = '0'
+    dfs(grid, row + 1, col)
+    dfs(grid, row, col + 1)
+    dfs(grid, row - 1, col)
+    dfs(grid, row, col - 1)
