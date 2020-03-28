b83847274bf48ffa1b180c3cc0593051d75f561f
satojkovic <satojkovic@gmail.com>
2020-03-28 09:49:51 +0900

    Add bfs

--- problems/num_islands.py
+++ problems/num_islands.py
@@ -15,3 +15,16 @@ def dfs(grid, row, col):
     dfs(grid, row, col + 1)
     dfs(grid, row - 1, col)
     dfs(grid, row, col - 1)
+
+from collections import deque
+def bfs(grid, row, col):
+    q = deque([(row, col)])
+    rows = len(grid)
+    cols = len(grid[0])
+    while q:
+        r, c = q.popleft()
+        grid[r][c] = '0'
+        for delta_r, delta_c in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+            if 0 <= r + delta_r < rows and 0 <= c + delta_c < cols and grid[r + delta_r][c + delta_c] == '1':
+                q.append((r + delta_r, c + delta_c))
+                grid[r + delta_r][c + delta_c] = '0'
