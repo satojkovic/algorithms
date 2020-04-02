ac5e8bfac7eb73a5fe598a039aa6d54fb77a9f90
satojkovic <satojkovic@gmail.com>
2020-04-02 21:34:57 +0900

    Knapsack problem by brute force

--- problems/power_set.py
+++ problems/power_set.py
@@ -9,6 +9,29 @@ def power_set(S):
         return ps
     return helper(S, 0)
 
+def total_weight(cand):
+    return sum([weight for weight, value in cand])
+
+def sales_value(cand):
+    return sum([value for weight, value in cand])
+
+def knapsack_brute_force(items, max_weight):
+    best_value = 0
+    best_cand = []
+    for cand in power_set(items):
+        if total_weight(cand) <= max_weight:
+            if sales_value(cand) > best_value:
+                best_value = sales_value(cand)
+                best_cand = cand
+    return best_cand
+
 if __name__ == "__main__":
     ps = power_set(['a', 'b', 'c'])
     print(ps)
+
+    # (weight, value)
+    res = knapsack_brute_force([(2, 4), (2, 5), (1, 2), (3, 8)], 5)
+    print(sum([value for weight, value in res]))
+
+    res = knapsack_brute_force([(9, 5), (10, 4)], 20)
+    print(sum([value for weight, value in res]))
