b16525f3692364f6afc9b37a7f25410bc45fff97
satojkovic <satojkovic@gmail.com>
2020-09-08 20:43:20 +0900

    Update bitarray.py

--- bitarray.py
+++ bitarray.py
@@ -6,3 +6,17 @@ class BitArray:
             int_size += 1
         self.bit_array = array.array('L') # unsigned 32 bit integer
         self.bit_array.extend((0,) * int_size)
+
+    # Set an integer with the bit at 'bit_num' to 1
+    def set(self, bit_num):
+        record = bit_num >> 5
+        offset = bit_num & 31
+        mask = 1 << offset
+        self.bit_array[record] |= mask
+
+    # Returns non-zero result if the bit at 'bit_num' is set to 1
+    def get(self, bit_num):
+        record = bit_num >> 5
+        offset = bit_num & 31
+        mask = 1 << offset
+        return self.bit_array[record] & mask
