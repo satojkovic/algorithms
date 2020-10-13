72c55e3945f27503908b028c1eeb8ffe62222baa
satojkovic <satojkovic@gmail.com>
2020-10-13 22:18:24 +0900

    Update

--- problems/delete_middle_node.py
+++ problems/delete_middle_node.py
@@ -10,3 +10,31 @@ def delete_middle_node(node):
     node.val = next_node.val
     node.next = next_node.next
     return True
+
+def delete_middle_node2(head):
+    if head is None or head.next is None:
+        return False
+    slow = head
+    prev_slow = head
+    count = 0
+    while head:
+        if count and count % 2 == 0:
+            prev_slow = slow
+            slow = slow.next
+        head = head.next
+        count += 1
+    if slow.next and slow != prev_slow:
+        prev_slow.next = slow.next
+
+if __name__ == "__main__":
+    head = ListNode('a')
+    head.next = ListNode('b')
+    head.next.next = ListNode('c')
+    head.next.next.next = ListNode('d')
+    head.next.next.next.next = ListNode('e')
+    head.next.next.next.next.next = ListNode('f')
+    delete_middle_node2(head)
+    while head:
+        print(head.val)
+        head = head.next
+
