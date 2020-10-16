3406019e0589edbdd3f11aae24f6b1e1303c3be6
satojkovic <satojkovic@gmail.com>
2020-10-16 22:04:52 +0900

    Update partition.py

--- problems/partition.py
+++ problems/partition.py
@@ -29,12 +29,36 @@ def partition(head, x):
     before_tail.next = after_head
     return before_head
 
+def partition2(head, x):
+    new_head = head
+    new_tail = head
+    while head:
+        next_head = head.next
+        if head.val < x:
+            head.next = new_head
+            new_head = head
+        else:
+            new_tail.next = head
+            new_tail = head
+        head = next_head
+    new_tail.next = None
+    return new_head
+
 if __name__ == "__main__":
     head = ListNode(3)
     head.next = ListNode(5)
     head.next.next = ListNode(8)
     head.next.next.next = ListNode(2)
     partitioned = partition(head, 5)
+    while partitioned:
+        print(partitioned.val)
+        partitioned = partitioned.next
+    print('--')
+    head = ListNode(3)
+    head.next = ListNode(5)
+    head.next.next = ListNode(8)
+    head.next.next.next = ListNode(2)
+    partitioned = partition2(head, 5)
     while partitioned:
         print(partitioned.val)
         partitioned = partitioned.next
