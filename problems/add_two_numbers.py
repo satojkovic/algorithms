180f5f802c5f6550cb1194141c3a7c273e8a7fc8
satojkovic <satojkovic@gmail.com>
2020-10-19 21:50:41 +0900

    Update add_two_numbers.py

--- problems/add_two_numbers.py
+++ problems/add_two_numbers.py
@@ -22,3 +22,27 @@ def add_two_numbers(l1: ListNode, l2: ListNode) -> ListNode:
         head.next = ListNode(carry)
         head = head.next
     return res.next
+
+def add_two_numbers2(l1, l2):
+    if l1 is None or l2 is None:
+        return l1 if l2 is None else l2
+    head = None
+    prev_head = None
+    carry = -1
+    while l1 or l2 or carry:
+        # Calculate addition
+        total = l1.val if l1 else 0
+        total += l2.val if l2 else 0
+        total += carry if carry != -1 else 0
+        # Create node
+        node = ListNode(total % 10)
+        if prev_head:
+            prev_head.next = node
+        prev_head = node
+        if head is None:
+            head = node
+        # Prepare next iteration
+        carry = total // 10
+        l1 = l1.next if l1 else None
+        l2 = l2.next if l2 else None
+    return head
