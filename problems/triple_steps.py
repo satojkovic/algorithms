2cd99fc3becefe8167d4944a15e120473a972e12
satojkovic <satojkovic@gmail.com>
2020-11-06 21:25:04 +0900

    Add triple_steps.py

---
+++ problems/triple_steps.py
@@ -0,0 +1,28 @@
+from numpy.lib.twodim_base import tri
+
+
+def triple_steps_brute_force(n):
+    if n < 0:
+        return 0
+    elif n == 0:
+        return 1
+    else:
+        return triple_steps_brute_force(n - 1) + triple_steps_brute_force(n - 2) + triple_steps_brute_force(n - 3)
+
+def triple_steps(n):
+    def steps(n, memo):
+        if n < 0:
+            return 0
+        elif n == 0:
+            return 1
+        elif memo[n]:
+            return memo[n]
+        else:
+            memo[n] = steps(n - 1, memo) + steps(n - 2, memo) + steps(n - 3, memo)
+            return memo[n]
+    memo = [0 for _ in range(n + 1)]
+    return steps(n, memo)
+
+if __name__ == "__main__":
+    print(triple_steps_brute_force(5))
+    print(triple_steps(5))
