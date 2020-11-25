131f13c948b584efae4526a327738b364d3a4e86
satojkovic <satojkovic@gmail.com>
2020-11-25 22:06:40 +0900

    Update perm.py

--- problems/perm.py
+++ problems/perm.py
@@ -7,3 +7,15 @@ def perm1(nums):
         perm = perm1(nums[:i] + nums[i+1:])
         ret += [[pivot] + p for p in perm]
     return ret
+
+def perm2(s):
+    if len(s) == 0:
+        return []
+    if len(s) == 1:
+        # type(s): list
+        return [s]
+    ret = []
+    for i, ch in enumerate(s):
+        res = perm2(s[:i] + s[i+1:])
+        ret.extend([[ch] + r for r in res])
+    return ret
--- problems/test_perm.py
+++ problems/test_perm.py
@@ -9,5 +9,12 @@ class TestPerm(unittest.TestCase):
         eq_(perm1([100]), [[100]])
         eq_(perm1([456, 456]), [[456, 456], [456, 456]])
 
+    def test_perm2(self):
+        eq_(perm2(['a', 'b', 'c']),
+                [['a', 'b', 'c'], ['a', 'c', 'b'], ['b', 'a', 'c'], ['b', 'c', 'a'],
+                ['c', 'a', 'b'], ['c', 'b', 'a']])
+        eq_(perm2([]), [])
+        eq_(perm2(['a']), [['a']])
+
 if __name__ == "__main__":
     unittest.main()
