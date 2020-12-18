429308451550fe7b4c287bd1cd64a2b5256d59d8
satojkovic <satojkovic@gmail.com>
2020-12-18 23:07:22 +0900

    Update frog.py

--- problems/frog.py
+++ problems/frog.py
@@ -12,5 +12,17 @@ def frog(N, costs):
             )
     return dp[N - 1]
 
+def frog2(N, costs):
+    def chmin(a, b):
+        return b if a > b else a
+    dp = [sys.maxsize] * N
+    dp[0] = 0
+    for i in range(1, N):
+        dp[i] = chmin(dp[i], dp[i - 1] + abs(costs[i] - costs[i - 1]))
+        if i > 1:
+            dp[i] = chmin(dp[i], dp[i - 2] + abs(costs[i] - costs[i - 2]))
+    return dp[N - 1]
+
 if __name__ == "__main__":
     print(frog(7, [2, 9, 4, 5, 1, 6, 10]))
+    print(frog2(7, [2, 9, 4, 5, 1, 6, 10]))
