e2cc11fcf16b65dcdc258ec0854162907c4e75d6
satojkovic <satojkovic@gmail.com>
2021-01-06 22:29:44 +0900

    Update heaps.py
    add pop function

--- python/heaps.py
+++ python/heaps.py
@@ -47,6 +47,7 @@ class MaxHeap:
 
     def push(self, val):
         self.data.append(val)
+        self.size += 1
         curr = len(self.data) - 1
         while curr > 0:
             parent = (curr - 1) // 2
@@ -54,6 +55,28 @@ class MaxHeap:
                 break
             self.data[parent], self.data[curr] = self.data[curr], self.data[parent]
             curr = parent
+
+    def pop(self):
+        if self.size == 0:
+            return None
+        self.data[0], self.data[-1] = self.data[-1], self.data[0]
+        ret = self.data.pop()
+        self.size -= 1
+        curr = 0
+        data_size = len(self.data)
+        while curr < data_size:
+            left_idx = 2 * curr + 1
+            right_idx = 2 * curr + 2
+            largest = curr
+            if left_idx < data_size and self.data[largest] < self.data[left_idx]:
+                largest = left_idx
+            if right_idx < data_size and self.data[largest] < self.data[right_idx]:
+                largest = right_idx
+            if largest == curr:
+                break
+            self.data[largest], self.data[curr] = self.data[curr], self.data[largest]
+            curr = largest
+        return ret
 
 class MinHeap:
     def __init__(self):
@@ -82,6 +105,7 @@ class MinHeap:
 
     def push(self, val):
         self.data.append(val)
+        self.size += 1
         curr = len(self.data) - 1
         while curr > 0:
             parent = (curr - 1) // 2
@@ -89,16 +113,44 @@ class MinHeap:
                 break
             self.data[parent], self.data[curr] = self.data[curr], self.data[parent]
             curr = parent
- 
+
+    def pop(self):
+        if self.size == 0:
+            return None
+        self.data[0], self.data[-1] = self.data[-1], self.data[0]
+        ret = self.data.pop()
+        self.size -= 1
+        curr = 0
+        data_size = len(self.data)
+        while curr < data_size:
+            left_idx = 2 * curr + 1
+            right_idx = 2 * curr + 2
+            smallest = curr
+            if left_idx < data_size and self.data[smallest] > self.data[left_idx]:
+                smallest = left_idx
+            if right_idx < data_size and self.data[smallest] > self.data[right_idx]:
+                smallest = right_idx
+            if smallest == curr:
+                break
+            self.data[smallest], self.data[curr] = self.data[curr], self.data[smallest]
+            curr = smallest
+        return ret
+
 if __name__ == "__main__":
     max_heap = MaxHeap()
     max_heap.build([6, 5, 1, 8, 2, 4])
     print(max_heap.data)
     max_heap.push(10)
     print(max_heap.data)
+    for i in range(len(max_heap.data)):
+        print('pop()=>{}'.format(max_heap.pop()))
+    print('pop()=>{}'.format(max_heap.pop()))
 
     min_heap = MinHeap()
     min_heap.build([6, 5, 1, 8, 2, 4])
     print(min_heap.data)
     min_heap.push(0)
     print(min_heap.data)
+    for i in range(len(min_heap.data)):
+        print('pop()=>{}'.format(min_heap.pop()))
+    print('pop()=>{}'.format(min_heap.pop()))
