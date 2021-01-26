0afec511a0ded8677c2a53f1980d9a68c6d84ac7
satojkovic <satojkovic@gmail.com>
2021-01-26 22:33:17 +0900

    Add longest_common_subsequence method

--- python/dynamic_programming.py
+++ python/dynamic_programming.py
@@ -29,9 +29,24 @@ def divide(elems, costs):
             dp[i] = min(dp[i], dp[j] + costs[j][i])
     return dp[N]
 
+def longest_common_subsequence(s, t):
+    dp = [[0] * (len(t) + 1) for _ in range(len(s) + 1)]
+    for i in range(1, len(s) + 1):
+        dp[i][0] = 0
+    for i in range(1, len(s) + 1):
+        for j in range(1, len(t) + 1):
+            if s[i - 1] == t[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    return dp[len(s)][len(t)]
+
+
 if __name__ == "__main__":
     items = [(3, 2), (4, 3), (1, 2), (2, 3), (3, 6)]
     print(knapsack(items, max_weight=10))
 
     print(edit_distance('saka', 'ana'))
     print(edit_distance('logistic', 'algorithm'))
+
+    print('LCS(abcde, ace) =', longest_common_subsequence('abcde', 'ace'))
