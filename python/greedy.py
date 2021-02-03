7932c6a8d1f4b409da66c8e3b490574e44283c87
satojkovic <satojkovic@gmail.com>
2021-02-03 21:55:37 +0900

    Update greedy.py

--- python/greedy.py
+++ python/greedy.py
@@ -8,6 +8,15 @@ def scheduling(intervals):
             current_end = reqs[i][1]
     return ans
 
+def multiple_array(A, B):
+    N = len(A)
+    total = 0
+    for i in range(N - 1, -1 , -1):
+        A[i] += total
+        count = 0 if A[i] % B[i] == 0 else B[i] - (A[i] % B[i])
+        total += count
+    return total
+
 if __name__ == "__main__":
     intervals = [[9, 16], [10, 12], [11, 15], [13, 19], [15, 18], [19, 23]]
     print(scheduling(intervals))
