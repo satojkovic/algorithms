44e66ee48216e26da6595c3c58c8f89384e51611
satojkovic <satojkovic@gmail.com>
2021-02-08 12:22:59 +0900

    Update test_graphs.py
    Add test case to test_check_biparty_graph

--- python/test_graphs.py
+++ python/test_graphs.py
@@ -23,6 +23,8 @@ class TestGraphs(unittest.TestCase):
     def test_check_biparty_graph(self):
         g = {0: [1, 3], 1: [0, 2, 4], 2: [1], 3: [0, 4], 4: [1, 3]}
         eq_(check_biparty_graph(g, 0), True)
+        g = {1: [2], 2: [1, 3], 3: [2, 4], 4: [3]}
+        eq_(check_biparty_graph(g, 1), True)
 
     def test_dfs_r(self):
         g = {0: [1, 4, 5], 1: [3, 4], 2: [1], 3: [2, 4], 4: [], 5: []}
