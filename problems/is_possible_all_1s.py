f4f7e5b3c83f5a3e1da2c387a3438ce549f4fe87
satojkovic <satojkovic@gmail.com>
2021-02-18 22:29:49 +0900

    Update is_possible_all_1s.py

--- problems/is_possible_all_1s.py
+++ problems/is_possible_all_1s.py
@@ -12,9 +12,24 @@ def is_possible(target):
         target[max_elem_i] = before
         return is_possible(target)
 
+def is_possible_iter(target):
+    while sum(target) != len(target):
+        max_elem = max(target)
+        max_elem_i = target.index(max_elem)
+        rest = sum(target[:max_elem_i])
+        rest = rest + sum(target[max_elem_i + 1:]) if max_elem_i != len(target) - 1 else rest
+        before = max_elem - rest
+        if before < 1:
+            return False
+        target[max_elem_i] = before
+    return True
+
 if __name__ == '__main__':
     print(is_possible([8, 5]))
+    print(is_possible_iter([8, 5]))
     print(is_possible([1, 1, 1, 2]))
+    print(is_possible_iter([1, 1, 1, 2]))
     print(is_possible([9, 3, 5]))
+    print(is_possible_iter([9, 3, 5]))
 
         
