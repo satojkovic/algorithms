8fd34a92c180b51e567d4e620dcf94e4f4315972
satojkovic <satojkovic@gmail.com>
2021-03-16 22:13:24 +0900

    Add new file
    RoundB: Bike Tour

---
+++ kickstart/RoundB/bike_tour.py
@@ -0,0 +1,13 @@
+def bike_tour():
+    T = int(input())
+    for t in range(1, T + 1):
+        N = int(input())
+        ckpts = list(map(int, input().split()))
+        res = 0
+        for i in range(1, N - 1):
+            if ckpts[i - 1] < ckpts[i] and ckpts[i] > ckpts[i + 1]:
+                res += 1
+        print('Case #{}: {}'.format(t, res))
+
+if __name__ == '__main__':
+    bike_tour()
