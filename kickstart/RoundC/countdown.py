352fcb9bf5230213b83c6b223b3d7caf76e07fff
satojkovic <satojkovic@gmail.com>
2021-04-06 21:37:10 +0900

    Update

--- kickstart/RoundC/countdown.py
+++ kickstart/RoundC/countdown.py
@@ -8,31 +8,31 @@ def countdown0():
         res = 0
         i = 0
         while i + K <= N:
-            if all([True if data[i+j]==K-j else False for j in range(K)]):
+            if all([True if data[i+j] == K-j else False for j in range(K)]):
                 res += 1
                 i = i + K
             else:
                 i += 1
         print('Case #{}: {}'.format(t, res))
 
-def countdown(N, K, data):
-    res = 0
-    i = N - 1
-    while i >= 0:
-        if data[i] != 1:
-            i -= 1
-            continue
-        j = i - 1
-        up = 1
-        while j >= 0:
-            if data[i] + up != data[j]:
-                break
-            up += 1
-            j -= 1
-        if up >= K:
-            res += 1
-        i = j
-    print('Case #: {}'.format(res))
+# Test set1: Passed
+# Test set2: Passed
+def countdown():
+    T = int(input())
+    for t in range(1, T + 1):
+        N, K = list(map(int, input().split()))
+        data = list(map(int, input().split()))
+        res = 0
+        dc = 0
+        for i in range(1, N):
+            if data[i] == data[i-1] - 1:
+                dc += 1
+            else:
+                dc = 0
+            if data[i] == 1 and dc >= K - 1:
+                res += 1
+        print('Case #{}: {}'.format(t, res))
+
 
 if __name__ == '__main__':
-    countdown0()
+    countdown()
