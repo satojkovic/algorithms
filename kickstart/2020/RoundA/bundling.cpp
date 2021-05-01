e443e0cd64b2ad220478e2cd87be436a2dc0a668
satojkovic <satojkovic@gmail.com>
2021-05-01 15:01:42 +0900

    Update

--- kickstart/2020/RoundA/bundling.cpp
+++ kickstart/2020/RoundA/bundling.cpp
@@ -1,14 +1,13 @@
-#include <iostream>
-#include <string>
-#include <utility>
+#include <bits/stdc++.h>
 
+using namespace std;
 typedef struct _TrieNode
 {
     _TrieNode *child[26];
     int count = 0;
 } TrieNode;
 
-void insert(TrieNode *root, std::string &s)
+void insert(TrieNode *root, string &s)
 {
     TrieNode *curr = root;
     for (int i = 0; i < s.size(); ++i)
@@ -23,7 +22,7 @@ void insert(TrieNode *root, std::string &s)
     curr->count++;
 }
 
-std::pair<int, int> calc_score(TrieNode *root, int depth, int K)
+pair<int, int> calc_score(TrieNode *root, int depth, int K)
 {
     int curr_score = 0;
     int curr_count = 0;
@@ -31,7 +30,7 @@ std::pair<int, int> calc_score(TrieNode *root, int depth, int K)
     {
         if (root->child[i])
         {
-            std::pair<int, int> score_count = calc_score(root->child[i], depth + 1, K);
+            pair<int, int> score_count = calc_score(root->child[i], depth + 1, K);
             curr_score += score_count.first;
             curr_count += score_count.second;
         }
@@ -42,29 +41,29 @@ std::pair<int, int> calc_score(TrieNode *root, int depth, int K)
         curr_count -= K;
         curr_score += depth;
     }
-    return std::make_pair(curr_score, curr_count);
+    return make_pair(curr_score, curr_count);
 }
 
 int main()
 {
-    int T = 0;
-    std::cin >> T;
+    int T;
+    cin >> T;
     for (int t = 1; t <= T; ++t)
     {
         TrieNode *root = new TrieNode();
-        int N = 0;
-        int K = 0;
-        std::string strings[N];
-        std::cin >>
+        int N, K;
+        cin >>
             N >> K;
+        string strings[N];
         for (int i = 0; i < N; ++i)
         {
-            std::cin >> strings[i];
+            cin >> strings[i];
             insert(root, strings[i]);
         }
-        std::pair<int, int> score_count = calc_score(root, 0, K);
-        std::cout
-            << "Case #" << t << ": " << score_count.first << std::endl;
+        pair<int, int> score_count = calc_score(root, 0, K);
+        cout
+            << "Case #" << t << ": " << score_count.first << endl;
         delete root;
     }
+    return 0;
 }
