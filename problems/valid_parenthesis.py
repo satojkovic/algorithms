55328717b58c08c842e13b8bef934f4e349f7755
satojkovic <satojkovic@gmail.com>
2021-05-31 21:59:14 +0900

    Add valid_parenthesis_aster
    Check if s is valid, where s contains '(', ')' and '*'.

--- problems/valid_parenthesis.py
+++ problems/valid_parenthesis.py
@@ -13,8 +13,28 @@ def valid_parenthesis(s):
     return len(stack) == 0
 
 
+def valid_parenthesis_aster(s):
+    # (*) is valid?
+    cmin, cmax = 0, 0
+    for c in s:
+        if c == '(':
+            cmin += 1
+            cmax += 1
+        if c == ')':
+            cmin = max(cmin - 1, 0)
+            cmax -= 1
+        if c == '*':
+            cmin = max(cmin - 1, 0)
+            cmax += 1
+        if cmax < 0:
+            return False
+    return cmin == 0
+
+
 if __name__ == '__main__':
     print(valid_parenthesis('[()]{}{[()()]()}'))
     print(valid_parenthesis('[(])'))
     print(valid_parenthesis('}}{{'))
     print(valid_parenthesis(''))
+
+    print(valid_parenthesis_aster('(*)'))
