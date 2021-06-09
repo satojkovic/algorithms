a00a41f9457063c9639d16188769d072023862a1
satojkovic <satojkovic@gmail.com>
2021-06-09 21:38:07 +0900

    Delete duplicate line

--- problems/sort_char_freq.py
+++ problems/sort_char_freq.py
@@ -3,7 +3,6 @@ from collections import Counter
 
 def sort_char_freq(s):
     char_freqs = Counter(s)
-    char_freqs = Counter(s)
     char_freqs = [item for item in char_freqs.items()]
     char_freqs = sorted(char_freqs, key=lambda x: -x[1])
     return ''.join([item[0]*item[1] for item in char_freqs])
