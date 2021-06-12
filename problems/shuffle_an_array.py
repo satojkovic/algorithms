5ae32847653e1ebdfda724cc48621784f77e1e48
satojkovic <satojkovic@gmail.com>
2021-06-12 22:17:56 +0900

    Add shuffle method
    Interwind former half and latter half of the array.

--- problems/shuffle_an_array.py
+++ problems/shuffle_an_array.py
@@ -18,3 +18,15 @@ class ShuffleAnArray:
             j = random.randrange(0, i+1) # end is exclusive unlike randint
             ret[i], ret[j] = ret[j], ret[i]
         return ret
+
+
+def shuffle(nums, n):
+    res = []
+    for i in range(n):
+        res.append(nums[i])
+        res.append(nums[i+n])
+    return res
+
+
+def test_shuffle():
+    assert shuffle([2, 5, 1, 3, 4, 7], 3) == [2, 3, 5, 4, 1, 7]
