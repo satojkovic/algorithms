a6e37e01d50f78d7a6328b2480cf624588d7e732
satojkovic <satojkovic@gmail.com>
2021-08-18 22:17:29 +0900

    Clean up

--- kickstart/2020/RoundD/record_breaker.py
+++ kickstart/2020/RoundD/record_breaker.py
@@ -1,21 +1,12 @@
-def record_breaker():
-    T = int(input())
-    for t in range(1, T + 1):
-        N = int(input())
-        vs = list(map(int, input().split(' ')))
-        prev_max = -1
-        res = 0
-        for i in range(N):
-            if i == N - 1:
-                if prev_max < vs[i]:
-                    res += 1
-                    prev_max = vs[i]
-            else:
-                if prev_max < vs[i] and vs[i] > vs[i+1]:
-                    res +=1
-                if prev_max < vs[i]:
-                    prev_max = vs[i]
-        print('Case #{}: {}'.format(t, res))
-
-if __name__ == '__main__':
-    record_breaker()
+T = int(input())
+for t in range(1, T + 1):
+    N = int(input())
+    visitors = list(map(int, input().split(' ')))
+    prev_record = 0
+    res = 0
+    for i in range(N):
+        greater_than_prev = i == 0 or visitors[i] > prev_record
+        greater_than_next = i == N - 1 or visitors[i] > visitors[i + 1]
+        res = res + 1 if greater_than_prev and greater_than_next else res
+        prev_record = max(prev_record, visitors[i])
+    print('Case #{}: {}'.format(t, res))
