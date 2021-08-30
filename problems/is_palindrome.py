5e5fd60ebd78e5b7e8e5d8cd3d32edb8a9ddcf8c
satojkovic <satojkovic@gmail.com>
2021-08-30 22:10:27 +0900

    Add new palindrom checker method

--- problems/is_palindrome.py
+++ problems/is_palindrome.py
@@ -10,6 +10,25 @@ def is_palindrome(s):
     return True
 
 
+def is_palindrome2(x):
+    # Time complexity: O(log10(x))
+    # Space complexity: O(1)
+    if x < 0 or (x % 10 == 0 and x != 0):
+        return False
+    revert_num = 0
+    while x > revert_num:
+        revert_num = revert_num * 10 + x % 10
+        x = x // 10
+    return x == revert_num or x == revert_num // 10
+
+
+def test_is_palindrome2():
+    assert is_palindrome2(12321) == True
+    assert is_palindrome2(120) == False
+    assert is_palindrome2(1221) == True
+    assert is_palindrome2(-121) == False
+
+
 if __name__ == '__main__':
     print(is_palindrome('aba'))
     print(is_palindrome('1234321'))
