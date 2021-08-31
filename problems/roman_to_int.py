da134e43a96d9b2d41cce66b8c643dd49963b203
satojkovic <satojkovic@gmail.com>
2021-08-31 22:43:52 +0900

    Auto format

--- problems/roman_to_int.py
+++ problems/roman_to_int.py
@@ -8,20 +8,27 @@
 #
 
 def roman_to_int1(s):
-    roman2int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    # Time complexity: O(n)
+    #  we iterate over an input string, nums. (length: n)
+    # Space complexity: O(n)
+    #  we create new list nums.
+    roman2int = {'I': 1, 'V': 5, 'X': 10,
+                 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     nums = [roman2int[ch] for ch in s]
     for i in range(0, len(nums) - 1, 1):
         if nums[i] < nums[i+1]:
             nums[i] *= -1
     return sum(nums)
 
-# Time complexity: O(n)
-#  we iterate over an input string once (but sum all numbers).
-#  append op costs O(1) at average case.
-# Space complexity: O(n)
-#  we create new list nums.
+
 def roman_to_int2(s):
-    roman2int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    # Time complexity: O(n)
+    #  we iterate over an input string once (but sum all numbers).
+    #  append op costs O(1) at average case.
+    # Space complexity: O(n)
+    #  we create new list nums.
+    roman2int = {'I': 1, 'V': 5, 'X': 10,
+                 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     nums = []
     for i, ch in enumerate(s):
         if i == len(s) - 1:
@@ -33,10 +40,12 @@ def roman_to_int2(s):
                 nums.append(roman2int[ch])
     return sum(nums)
 
-# Time complexity: O(n)
-# Space complexity: O(1)
+
 def roman_to_int3(s):
-    roman2int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    # Time complexity: O(n)
+    # Space complexity: O(1)
+    roman2int = {'I': 1, 'V': 5, 'X': 10,
+                 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     ret = 0
     for i, ch in enumerate(s):
         if i == len(s) - 1:
@@ -47,3 +56,5 @@ def roman_to_int3(s):
             else:
                 ret += roman2int[ch]
     return ret
+
+
