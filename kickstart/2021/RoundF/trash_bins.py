b58d481929f1762c5ba46eb5a36f16833e8680f3
satojkovic <satojkovic@gmail.com>
2021-09-19 22:37:02 +0900

    Improved solution for trash bins problem
    Test set1: Passed
    Test set2: Passed

--- kickstart/2021/RoundF/trash_bins.py
+++ kickstart/2021/RoundF/trash_bins.py
@@ -16,16 +16,55 @@ def right_diff(trash_bins, i):
     return N if right == len(trash_bins) else abs(right - i)
 
 
-T = int(input())
-for t in range(1, T + 1):
-    N = int(input())
-    trash_bins = input()
-    trash_bins = list(map(int, [c for c in trash_bins]))
-    ret = 0
-    for i in range(N):
-        if trash_bins[i] == 1:
-            continue
-        l = left_diff(trash_bins, i)
-        r = right_diff(trash_bins, i)
-        ret += min(l, r)
-    print('Case #{}: {}'.format(t, ret))
+def solve1():
+    T = int(input())
+    for t in range(1, T + 1):
+        N = int(input())
+        trash_bins = input()
+        trash_bins = list(map(int, [c for c in trash_bins]))
+        ret = 0
+        for i in range(N):
+            if trash_bins[i] == 1:
+                continue
+            l = left_diff(trash_bins, i)
+            r = right_diff(trash_bins, i)
+            ret += min(l, r)
+        print('Case #{}: {}'.format(t, ret))
+
+
+def calc_score(l, r, N):
+    if r == 0:
+        return 0
+    elif l == -1:
+        return (r * (r + 1)) // 2
+    elif r == -1:
+        return ((N-l-1) * (N-l)) // 2
+    else:
+        zero_count = r - l - 1
+        if zero_count == 1:
+            return 1
+        elif zero_count == 2:
+            return 2
+        else:
+            n = zero_count // 2
+            return n * (n+1) if zero_count % 2 == 0 else n * (n+1) + (n+1)
+
+
+def solve2():
+    T = int(input())
+    for t in range(1, T + 1):
+        N = int(input())
+        trash_bins = input()
+        trash_bins = list(map(int, [c for c in trash_bins]))
+        l = -1
+        ret = 0
+        for r in range(N):
+            if trash_bins[r] == 1:
+                ret += calc_score(l, r, N)
+                l = r
+        print('Case #{}: {}'.format(t, ret if l ==
+              N - 1 else ret + calc_score(l, -1, N)))
+
+
+if __name__ == '__main__':
+    solve2()
