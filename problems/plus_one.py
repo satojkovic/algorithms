ff0aa24857aca1dc69922dd9d61f2e81f538faac
satojkovic <satojkovic@gmail.com>
2021-09-21 22:30:45 +0900

    Add new solution for plus one problem

--- problems/plus_one.py
+++ problems/plus_one.py
@@ -6,3 +6,15 @@ def plus_one(digits):
         res[i] = digit % 10
         carry = digit // 10
     return res if carry == 0 else [1] + res
+
+
+def plus_one2(digits):
+    n = int(''.join([str(d) for d in digits]))
+    n += 1
+    return [int(a) for a in str(n)]
+
+
+def test_plus_one2():
+    assert plus_one2([1, 2, 3]) == [1, 2, 4]
+    assert plus_one2([9, 9]) == [1, 0, 0]
+    assert plus_one2([0]) == [1]
