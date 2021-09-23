6730c57d9dac8b5812484e0bc7531b1ce790f735
satojkovic <satojkovic@gmail.com>
2021-09-23 20:49:35 +0900

    Use digits directly

--- problems/plus_one.py
+++ problems/plus_one.py
@@ -1,11 +1,10 @@
 def plus_one(digits):
-    res = len(digits) * [0]
     carry = 1
     for i in reversed(range(len(digits))):
         digit = digits[i] + carry
-        res[i] = digit % 10
+        digits[i] = digit % 10
         carry = digit // 10
-    return res if carry == 0 else [1] + res
+    return digits if carry == 0 else [1] + digits
 
 
 def plus_one2(digits):
