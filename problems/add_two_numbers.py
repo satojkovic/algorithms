726f0032718b25f565773deec42761e1af274c99
satojkovic <satojkovic@gmail.com>
2021-09-28 22:57:23 +0900

    Fix iterative and recursive solution for add_two_numbers

--- problems/add_two_numbers.py
+++ problems/add_two_numbers.py
@@ -3,60 +3,36 @@ class ListNode:
         self.val = val
         self.next = None
 
+
 def add_two_numbers(l1, l2):
-    if l1 is None or l2 is None:
-        return l1 if l2 is None else l2
     dummy = ListNode(None)
     head = dummy
     carry = 0
     while l1 or l2:
-        total = l1.val if l1 else 0
-        total += l2.val if l2 else 0
-        total += carry
+        l1_val = l1.val if l1 else 0
+        l2_val = l2.val if l2 else 0
+        total = l1_val + l2_val + carry
         head.next = ListNode(total % 10)
         carry = total // 10
         head = head.next
-        l1 = l1.next if l1 else None
-        l2 = l2.next if l2 else None
-    if carry != 0:
+        l1 = l1.next if l1 else l1
+        l2 = l2.next if l2 else l2
+    if carry:
         head.next = ListNode(carry)
         head = head.next
     return dummy.next
 
-def add_two_numbers2(l1, l2):
-    if l1 is None or l2 is None:
-        return l1 if l2 is None else l2
-    head = None
-    prev_head = None
-    carry = -1
-    while l1 or l2 or carry:
-        # Calculate addition
-        total = l1.val if l1 else 0
-        total += l2.val if l2 else 0
-        total += carry if carry != -1 else 0
-        # Create node
-        node = ListNode(total % 10)
-        if prev_head:
-            prev_head.next = node
-        prev_head = node
-        if head is None:
-            head = node
-        # Prepare next iteration
-        carry = total // 10
-        l1 = l1.next if l1 else None
-        l2 = l2.next if l2 else None
-    return head
 
-def add_two_numbers3(l1, l2):
+def add_two_numbers_recursion(l1, l2):
     def addition(l1, l2, carry):
         if l1 is None and l2 is None and carry == 0:
             return None
-        total = l1.val if l1 else 0
-        total += l2.val if l2 else 0
-        total += carry
+        l1_val = l1.val if l1 else 0
+        l2_val = l2.val if l2 else 0
+        total = l1_val + l2_val + carry
         node = ListNode(total % 10)
-        l1 = l1.next if l1 else None
-        l2 = l2.next if l2 else None
+        l1 = l1.next if l1 else l1
+        l2 = l2.next if l2 else l2
         node.next = addition(l1, l2, total // 10)
         return node
     return addition(l1, l2, 0)
