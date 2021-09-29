b003692c582e6f4241a37ffd3eafa0c2f1570dce
satojkovic <satojkovic@gmail.com>
2021-09-29 23:19:39 +0900

    Replace import with class definition

--- problems/replace_words.py
+++ problems/replace_words.py
@@ -1,6 +1,39 @@
-import sys
-sys.path.append('..')
-from trie import Trie, TrieNode
+class TrieNode:
+    def __init__(self, data):
+        self.data = data
+        self.children = {}
+        self.word_finished = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode('')
+
+    def insert(self, s):
+        def _insert(root, s, pos):
+            if len(s) - 1 == pos:
+                if not s[pos] in root.children:
+                    root.children[s[pos]] = TrieNode(root.data + s[pos])
+                root.children[s[pos]].word_finished = True
+                return root
+
+            if not s[pos] in root.children:
+                root.children[s[pos]] = TrieNode(root.data + s[pos])
+            root.children[s[pos]] = _insert(root.children[s[pos]], s, pos + 1)
+            return root
+        self.root = _insert(self.root, s, 0)
+
+    def replace_with_prefix(self, word):
+        head = self.root
+        return self._replace_with_prefix(head, word)
+
+    def _replace_with_prefix(self, head, word):
+        if len(word) == 0 or head.word_finished:
+            return head.data
+        if not word[0] in head.children:
+            return None
+        return self._replace_with_prefix(head.children[word[0]], word[1:])
+
 
 def replace_words(dict, sentence):
     trie = Trie()
