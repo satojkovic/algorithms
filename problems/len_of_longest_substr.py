9f40a77b22cebd273da032ef4112a755ff478e9a
satojkovic <satojkovic@gmail.com>
2021-10-09 21:49:58 +0900

    Add other solution for length of longest substring problem

--- problems/len_of_longest_substr.py
+++ problems/len_of_longest_substr.py
@@ -53,3 +53,32 @@ def test_brute_force():
     assert brute_force('abcabcbb') == 3
     assert brute_force('bbbbb') == 1
     assert brute_force('abcadef') == 6
+
+
+def sliding_window(s):
+    from collections import defaultdict
+    left, right = 0, 0
+    seen = defaultdict(int)
+    ret = 0
+
+    while right < len(s):
+        # Move right until finding duplicate character.
+        # If duplicate character is found, left pointer is moved to the right
+        # until the count of current character is less equal than 1.
+        # (guranteed that there is no duplicate characters in the window)
+        seen[s[right]] += 1
+        while seen[s[right]] > 1:
+            seen[s[left]] -= 1
+            left += 1
+        ret = max(ret, right - left + 1)
+        right += 1
+    return ret
+
+
+def test_sliding_window():
+    assert sliding_window('abba') == 2
+    assert sliding_window('') == 0
+    assert sliding_window('s') == 1
+    assert sliding_window('abcabcbb') == 3
+    assert sliding_window('bbbbb') == 1
+    assert sliding_window('abcadef') == 6
