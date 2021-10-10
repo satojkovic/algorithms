a8993ce4709b6bbed5c4e0ba281a3f3715c09d63
satojkovic <satojkovic@gmail.com>
2021-10-10 11:27:46 +0900

    Clean up len_of_longest_substr method

--- problems/len_of_longest_substr.py
+++ problems/len_of_longest_substr.py
@@ -1,21 +1,20 @@
 def len_of_longest_substr(s):
-    if len(s) <= 1:
-        return len(s)
-    i = 0
-    seen = {s[0]: 0}
-    ret = 1
-    for j in range(1, len(s)):
+    i, ret = 0, 0
+    seen = {}
+    for j in range(len(s)):
         if s[j] in seen:
-            # Update ret value with distance between start pointer i and end pointer j
-            # only when the distance is greater than ret.
-            ret = max(ret, j - i)
             # Update the pointer i to a position that have no duplicate chars.
             # That is, it will be next to the position where the same character as the current position
-            # is found, or the current position, whichever is larger.
+            # is found, or the current position i, whichever is larger.
+            # (For example, 'abba', 'a' appears the first but reappears later than 'b'.)
+            # (In this case, max(i, seen[s[j]] + 1) == i)
             i = max(i, seen[s[j]] + 1)
+        # Update ret value with distance between start pointer i and end pointer j
+        # only when the distance is greater than ret.
+        ret = max(ret, j - i + 1)
         # New unique char's position is stored or existing char's position is updated.
         seen[s[j]] = j
-    return max(ret, len(s) - i)
+    return ret
 
 
 def test_len_of_longest_substr():
