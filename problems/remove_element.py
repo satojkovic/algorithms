c2a5acb1ce506d3621dbaaeffb6777961b61c084
satojkovic <satojkovic@gmail.com>
2021-10-18 22:52:08 +0900

    Fix test_remove_element_by_swapping_last

--- problems/remove_element.py
+++ problems/remove_element.py
@@ -28,8 +28,8 @@ def remove_element_by_swapping_last(nums, val):
 
 
 def test_remove_element_by_swapping_last():
-    assert remove_element([3, 2, 2, 3], 2) == 2
-    assert remove_element([0, 1, 2, 2, 3, 0, 4, 2], 2) == 5
-    assert remove_element([], 4) == 0
-    assert remove_element([1], 1) == 0
-    assert remove_element([1, 2, 3, 4], 5) == 4
+    assert remove_element_by_swapping_last([3, 2, 2, 3], 2) == 2
+    assert remove_element_by_swapping_last([0, 1, 2, 2, 3, 0, 4, 2], 2) == 5
+    assert remove_element_by_swapping_last([], 4) == 0
+    assert remove_element_by_swapping_last([1], 1) == 0
+    assert remove_element_by_swapping_last([1, 2, 3, 4], 5) == 4
