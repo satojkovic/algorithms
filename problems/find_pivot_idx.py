df89db480653e5e6139b11b09ae937de8feeb85d
satojkovic <satojkovic@gmail.com>
2021-10-21 23:17:20 +0900

    Add test method for find pivot index problem

--- problems/find_pivot_idx.py
+++ problems/find_pivot_idx.py
@@ -8,3 +8,10 @@ def find_pivot_idx(nums):
         if left == right:
             return i
     return -1
+
+
+def test_find_pivot_idx():
+    assert find_pivot_idx([1, 7, 3, 6, 5, 6]) == 3
+    assert find_pivot_idx([1, 2, 0, 0, 3]) == 2
+    assert find_pivot_idx([1]) == 0
+    assert find_pivot_idx([]) == -1
