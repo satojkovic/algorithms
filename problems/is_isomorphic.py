246b370d0f7828d6a062657eb40db160c3f28a79
satojkovic <satojkovic@gmail.com>
2021-10-24 20:29:02 +0900

    Add test

--- problems/is_isomorphic.py
+++ problems/is_isomorphic.py
@@ -9,3 +9,13 @@ def is_isomorphic(s, t):
         m[s[i]] = t[i]
         inv_m[t[i]] = s[i]
     return True
+
+
+def test_is_isomorphic():
+    assert is_isomorphic('add', 'egg') == True
+    assert is_isomorphic('fore', 'bare') == True
+    assert is_isomorphic('ab', 'aa') == False
+    assert is_isomorphic('akb', 'bkb') == False
+    assert is_isomorphic('a', 'a') == True
+    assert is_isomorphic('ccc', 'aaa') == True
+    assert is_isomorphic('', '') == True
