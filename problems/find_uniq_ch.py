4de2dea04a38c9e7d1b6c5b079939df4bdfd8073
satojkovic <satojkovic@gmail.com>
2021-10-26 22:39:03 +0900

    Add test method and fix some bugs

--- problems/find_uniq_ch.py
+++ problems/find_uniq_ch.py
@@ -1,11 +1,18 @@
 def find_uniq_ch(s):
     m = {}
     for i, ch in enumerate(s):
-        if not ch in m:
+        if ch not in m:
             m[ch] = [i]
             continue
         m[ch].append(i)
-    for ch in enumerate(s):
+    for _, ch in enumerate(s):
         if len(m[ch]) == 1:
             return m[ch][0]
     return -1
+
+
+def test_find_uniq_ch():
+    assert find_uniq_ch('abcaad') == 1
+    assert find_uniq_ch('a') == 0
+    assert find_uniq_ch('') == -1
+    assert find_uniq_ch('aaa') == -1
