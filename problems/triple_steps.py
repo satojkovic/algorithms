f8f82ccf70e914b7b7dde82cf6885f47a1295b45
satojkovic <satojkovic@gmail.com>
2021-12-05 23:26:06 +0900

    Update if conditions

--- problems/triple_steps.py
+++ problems/triple_steps.py
@@ -1,25 +1,25 @@
 def triple_steps_brute_force(n):
     if n < 0:
         return 0
-    elif n == 0:
+    if n == 0:
         return 1
-    else:
-        return triple_steps_brute_force(n - 1) + triple_steps_brute_force(n - 2) + triple_steps_brute_force(n - 3)
+    return triple_steps_brute_force(n - 1) + triple_steps_brute_force(n - 2) + triple_steps_brute_force(n - 3)
+
 
 def triple_steps(n):
     def steps(n, memo):
         if n < 0:
             return 0
-        elif n == 0:
+        if n == 0:
             return 1
-        elif memo[n]:
+        if memo[n]:
             return memo[n]
-        else:
-            memo[n] = steps(n - 1, memo) + steps(n - 2, memo) + steps(n - 3, memo)
-            return memo[n]
+        memo[n] = steps(n-1, memo) + steps(n-2, memo) + steps(n-3, memo)
+        return memo[n]
     memo = [0 for _ in range(n + 1)]
     return steps(n, memo)
 
-if __name__ == "__main__":
-    print(triple_steps_brute_force(5))
-    print(triple_steps(5))
+
+def test_triple_steps():
+    assert triple_steps_brute_force(5) == 13
+    assert triple_steps(5) == 13
