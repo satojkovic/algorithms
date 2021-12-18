694470d8801b68e0c95c5b17f6ecd567ac430c13
satojkovic <satojkovic@gmail.com>
2021-12-18 15:50:30 +0900

    Add pytest method for merge two sorted array problem

--- problems/merge_two_sorted_array.py
+++ problems/merge_two_sorted_array.py
@@ -10,4 +10,20 @@ def merge_two_sorted_array(a, b, a_size, b_size):
             a[last_merged] = b[last_b]
             last_b -= 1
         last_merged -= 1
-    return a
+
+
+def test_merge_two_sorted_array():
+    a = [1, 2, 3, 0, 0, 0]
+    b = [2, 5, 6]
+    merge_two_sorted_array(a, b, 3, 3)
+    assert a == [1, 2, 2, 3, 5, 6]
+
+    a = [1]
+    b = []
+    merge_two_sorted_array(a, b, 1, 0)
+    assert a == [1]
+
+    a = [0]
+    b = [1]
+    merge_two_sorted_array(a, b, 0, 1)
+    assert a == [1]
