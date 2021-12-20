0f1fed0a48cd84758726afff2fec298430f197c6
satojkovic <satojkovic@gmail.com>
2021-12-21 00:20:57 +0900

    Add is_balanced method

--- problems/check_balanced.py
+++ problems/check_balanced.py
@@ -4,6 +4,7 @@ class TreeNode:
         self.left = None
         self.right = None
 
+
 def check_balanced(root):
     if root is None:
         return True
@@ -13,6 +14,7 @@ def check_balanced(root):
     else:
         return check_balanced(root.left) and check_balanced(root.right)
 
+
 def get_height(root):
     if root is None:
         return -1
@@ -20,10 +22,28 @@ def get_height(root):
     right_height = get_height(root.right)
     return max(left_height, right_height) + 1
 
-if __name__ == "__main__":
+
+def is_balanced(root):
+    def _is_balanced(root):
+        if root is None:
+            return True, -1
+        left_cond, left_h = _is_balanced(root.left)
+        right_cond, right_h = _is_balanced(root.right)
+        return abs(left_h - right_h) <= 1 and left_cond and right_cond, max(left_h, right_h) + 1
+    cond, h = _is_balanced(root)
+    return cond
+
+
+def test_is_balanced():
     root = TreeNode(10)
     root.left = TreeNode(3)
     root.right = TreeNode(4)
     root.left.left = TreeNode(14)
+    cond = is_balanced(root)
+    assert cond == True
 
-    print(check_balanced(root))
+    root = TreeNode(10)
+    root.right = TreeNode(3)
+    root.right.right = TreeNode(4)
+    cond = is_balanced(root)
+    assert cond == False
