bd43637ddda0c4c443e94566de2202ac576c7726
satojkovic <satojkovic@gmail.com>
2021-12-23 23:12:34 +0900

    Update two_sum.py

--- problems/test_two_sum.py
+++ problems/test_two_sum.py
@@ -2,13 +2,10 @@ import unittest
 from two_sum import *
 from nose.tools import eq_
 
+
 class TestTwoSum(unittest.TestCase):
     def test_two_sum(self):
-        eq_(two_sum([1, 2, 7, 9], 9), [1, 2])
-        eq_(two_sum([2, 2, 5, 4], 6), [1, 3])
-        eq_(two_sum([1, 1, 1, 1, 1], 2), [0, 1])
-        eq_(two_sum([3, 7, 11], 2), [-1, -1])
-        eq_(two_sum([3, 2, 4], 6), [1, 2])
+
 
 if __name__ == "__main__":
     unittest.main()
--- problems/two_sum.py
+++ problems/two_sum.py
@@ -12,3 +12,31 @@ def two_sum(nums, target):
             return [seen[rest], i]
         seen[num] = i
     return [-1, -1]
+
+
+def two_sum_brute_force(nums, target):
+    for i in range(len(nums)):
+        for j in range(i, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    return [-1, -1]
+
+
+def two_sum_two_pass(nums, target):
+    seen = {}
+    for i in range(len(nums)):
+        seen[nums[i]] = i
+    for i in range(len(nums)):
+        rest = target - nums[i]
+        if rest in seen and seen[rest] != i:
+            return [seen[rest], i]
+    return [-1, -1]
+
+
+def test_two_sum():
+    assert two_sum([1, 2, 7, 9], 9) == [1, 2]
+    assert two_sum([2, 2, 5, 4], 6) == [1, 3]
+    assert two_sum([1, 1, 1, 1, 1], 2) == [0, 1]
+    assert two_sum([3, 7, 11], 2) == [-1, -1]
+    assert two_sum([3, 2, 4], 6) == [1, 2]
+    assert two_sum([0], 10) == [-1, -1]
