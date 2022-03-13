f052037e4b4fa0f93aec629a8460bd2ef3d31c77
satojkovic <satojkovic@gmail.com>
2022-03-13 18:04:22 +0900

    Use dict for storing dp values

--- codejam/2021/QR/moons_and_umbrellas.py
+++ codejam/2021/QR/moons_and_umbrellas.py
@@ -31,53 +31,53 @@ def moons_and_umbrellas_dp():
         X, Y, S = input().split()
         costCJ, costJC = int(X), int(Y)
         N = len(S)
-        dp = [0] * 2
-        prev_dp = [0] * 2
+        dp = {k: 0 for k in ["C", "J"]}
+        prev_dp = {k: 0 for k in ["C", "J"]}
         for m in range(N):
             if S[m] == "C" and m == 0:
-                dp[0] = 0
+                dp["C"] = 0
                 # Heavily penalise attempt to change hardwired C to J
-                dp[1] = INF
+                dp["J"] = INF
             if S[m] == "J" and m == 0:
                 # Heavily penalise attempt to change hardwired J to C
-                dp[0] = INF
-                dp[1] = 0
+                dp["C"] = INF
+                dp["J"] = 0
             if S[m] == "?" and m == 0:
-                dp[0] = 0
-                dp[1] = 0
+                dp["C"] = 0
+                dp["J"] = 0
 
             if S[m] == "C" and m > 0 and S[m-1] == "C":
-                dp[0] = prev_dp[0]
-                dp[1] = INF
+                dp["C"] = prev_dp["C"]
+                dp["J"] = INF
             if S[m] == "C" and m > 0 and S[m-1] == "J":
-                dp[0] = prev_dp[1]+costJC
-                dp[1] = INF
+                dp["C"] = prev_dp["J"]+costJC
+                dp["J"] = INF
             if S[m] == "J" and m > 0 and S[m-1] == "C":
-                dp[0] = INF
-                dp[1] = prev_dp[0]+costCJ
+                dp["C"] = INF
+                dp["J"] = prev_dp["C"]+costCJ
             if S[m] == "J" and m > 0 and S[m-1] == "J":
-                dp[0] = INF
-                dp[1] = prev_dp[1]
+                dp["C"] = INF
+                dp["J"] = prev_dp["J"]
 
             if S[m] == "?" and m > 0 and S[m-1] == "C":
-                dp[0] = prev_dp[0]
-                dp[1] = prev_dp[0]+costCJ
+                dp["C"] = prev_dp["C"]
+                dp["J"] = prev_dp["C"]+costCJ
             if S[m] == "?" and m > 0 and S[m-1] == "J":
-                dp[0] = prev_dp[1]+costJC
-                dp[1] = prev_dp[1]
+                dp["C"] = prev_dp["J"]+costJC
+                dp["J"] = prev_dp["J"]
 
             if S[m] == "C" and m > 0 and S[m-1] == "?":
-                dp[0] = min(prev_dp[0], prev_dp[1]+costJC)
-                dp[1] = INF
+                dp["C"] = min(prev_dp["C"], prev_dp["J"]+costJC)
+                dp["J"] = INF
             if S[m] == "J" and m > 0 and S[m-1] == "?":
-                dp[0] = INF
-                dp[1] = min(prev_dp[0]+costCJ, prev_dp[1])
+                dp["C"] = INF
+                dp["J"] = min(prev_dp["C"]+costCJ, prev_dp["J"])
             if S[m] == "?" and m > 0 and S[m-1] == "?":
-                dp[0] = min(prev_dp[0], prev_dp[1]+costJC)
-                dp[1] = min(prev_dp[0]+costCJ, prev_dp[1])
+                dp["C"] = min(prev_dp["C"], prev_dp["J"]+costJC)
+                dp["J"] = min(prev_dp["C"]+costCJ, prev_dp["J"])
 
-            prev_dp[0], prev_dp[1] = dp[0], dp[1]
-        print('Case #{}: {}'.format(t, min(dp[0], dp[1])))
+            prev_dp["C"], prev_dp["J"] = dp["C"], dp["J"]
+        print('Case #{}: {}'.format(t, min(dp["C"], dp["J"])))
 
 
 if __name__ == '__main__':
