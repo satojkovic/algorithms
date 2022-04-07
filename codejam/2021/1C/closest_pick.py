0ef54866cba6d46711cd3e5b7d942d6029800ffb
satojkovic <satojkovic@gmail.com>
2022-04-07 23:09:30 +0900

    Update solution of closest pick problem
    Codejam 2021, Round1C

--- codejam/2021/1C/closest_pick.py
+++ codejam/2021/1C/closest_pick.py
@@ -1,27 +1,43 @@
-def calc_prob(a, b, K, sold_tickets):
-    count = 0
-    for c in range(1, K + 1):
-        if c in sold_tickets:
-            continue
-        elif c == a or c == b:
-            count += 1
-        else:
-            diff_sold = min([abs(t-c) for t in sold_tickets])
-            diff_purchased = min(abs(a - c), abs(b - c))
-            if diff_sold > diff_purchased:
-                count += 1
-    return count / K
+# def calc_prob(a, b, K, sold_tickets):
+#     count = 0
+#     for c in range(1, K + 1):
+#         if c in sold_tickets:
+#             continue
+#         elif c == a or c == b:
+#             count += 1
+#         else:
+#             diff_sold = min([abs(t-c) for t in sold_tickets])
+#             diff_purchased = min(abs(a - c), abs(b - c))
+#             if diff_sold > diff_purchased:
+#                 count += 1
+#     return count / K
+
+# T = int(input())
+# for t in range(1, T + 1):
+#     N, K = list(map(int, input().split()))
+#     sold_tickets = list(map(int, input().split()))
+#     # Pick K^2 possible pairs and calc probability
+#     for i in range(1, K):
+#         for j in range(i+1, K+1):
+#             prob = calc_prob(i, j, K, sold_tickets)
+#             res = max(res, prob)
+#     print('Case #{}: {}'.format(t, res))
+
+def calc_prob(N, K, sold_tickets):
+    prob_a = [sold_tickets[0] - 1, K - sold_tickets[-1]]
+    prob_b = [sold_tickets[0] - 1, K - sold_tickets[-1]]
+    for i in range(N-1):
+        diff = sold_tickets[i+1] - sold_tickets[i]
+        prob_a.append(diff // 2)
+        prob_b.append(diff - 1)
+    prob_a.sort(reverse=True)
+    prob_b.sort(reverse=True)
+    return max((prob_a[0] + prob_a[1]) / K, prob_b[0] / K)
 
 
 T = int(input())
 for t in range(1, T + 1):
     N, K = list(map(int, input().split()))
     sold_tickets = list(map(int, input().split()))
-    sold_tickets = set(sold_tickets)
-    res = 0.0
-    # Pick K^2 possible pairs and calc probability
-    for i in range(1, K):
-        for j in range(i+1, K+1):
-            prob = calc_prob(i, j, K, sold_tickets)
-            res = max(res, prob)
-    print('Case #{}: {}'.format(t, res))
+    prob = calc_prob(N, K, sorted(sold_tickets))
+    print('Case #{}: {}'.format(t, prob))
