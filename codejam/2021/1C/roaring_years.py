06f66ead6ad90aef0bf81a96cdce234fd8d2b4e2
satojkovic <satojkovic@gmail.com>
2022-04-20 22:13:59 +0900

    Update solution of roaring years problem
    Check roaring years with 3 or more numbers are concatenated,
    and with 2 numbers concatenated separately.
    Finally return the smallest of these two subsets.

--- codejam/2021/1C/roaring_years.py
+++ codejam/2021/1C/roaring_years.py
@@ -1,24 +1,17 @@
 def get_roaring_years():
     import math
     ret = []
-    for i in range(1, 1000):
+    for i in range(1, 10**6):
         y = i
+        y_len = int(math.log10(y) + 1)
         digit = i
-        while y < 1234567:
+        while y_len < 20:
             digit += 1
             digit_len = int(math.log10(digit) + 1)
+            y_len += digit_len
             y = y * 10 ** digit_len + digit
             ret.append(y)
     return sorted(ret)
-
-
-def linear_search(y, years):
-    ret = 1234567
-    for year in years:
-        if year > y:
-            ret = year
-            break
-    return ret
 
 
 def concat(x):
@@ -27,7 +20,7 @@ def concat(x):
     return x * (10**digit_len) + (x + 1)
 
 
-def binary_search(y):
+def binary_search_two(y):
     left = 0
     right = 10**9 - 1
     while right - left > 1:
@@ -39,8 +32,22 @@ def binary_search(y):
     return concat(right)
 
 
+def binary_search_three(y, years):
+    left = -1
+    right = len(years) - 1
+    while right - left > 1:
+        mid = left + (right - left) // 2
+        if years[mid] > y:
+            right = mid
+        else:
+            left = mid
+    return years[right]
+
+
 T = int(input())
 for t in range(1, T + 1):
     Y = int(input())
     roaring_years = get_roaring_years()
-    print('Case #{}: {}'.format(t, linear_search(Y, roaring_years)))
+    print('Case #{}: {}'.format(
+        t, min(binary_search_two(Y), binary_search_three(Y, roaring_years)
+               )))
