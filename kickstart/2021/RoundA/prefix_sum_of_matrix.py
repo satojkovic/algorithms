b0cdbc042a0008773724801f1dd452ee2bd29b0d
satojkovic <satojkovic@gmail.com>
2022-09-10 15:33:03 +0900

    Add test case

--- kickstart/2021/RoundA/prefix_sum_of_matrix.py
+++ kickstart/2021/RoundA/prefix_sum_of_matrix.py
@@ -36,6 +36,22 @@ def test_cumsum_matrix():
     cumsum_mat = cumsum_matrix(mat)
     assert cumsum_mat == [[10]]
 
+    mat = [
+        [1, 2, 3, 4, 6],
+        [5, 3, 8, 1, 2],
+        [4, 6, 7, 5, 5],
+        [2, 4, 8, 9, 4]
+    ]
+    cumsum_mat = cumsum_matrix(mat)
+    assert cumsum_mat == [
+        [1, 3, 6, 10, 16],
+        [5, 8, 16, 17, 19],
+        [4, 10, 17, 22, 27],
+        [2, 6, 14, 23, 27]
+    ]
+
+
+
 def prefixsum_matrix(mat):
     rows, cols = len(mat), len(mat[0])
     cumsum_mat = cumsum_matrix(mat)
@@ -74,3 +90,17 @@ def test_prefixsum_matrix():
     mat = [[10]]
     prefixsum_mat = prefixsum_matrix(mat)
     assert prefixsum_mat == [[10]]
+
+    mat = [
+        [1, 2, 3, 4, 6],
+        [5, 3, 8, 1, 2],
+        [4, 6, 7, 5, 5],
+        [2, 4, 8, 9, 4]
+    ]
+    prefixsum_mat = prefixsum_matrix(mat)
+    assert prefixsum_mat == [
+        [1, 3, 6, 10, 16],
+        [6, 11, 22, 27, 35],
+        [10, 21, 39, 49, 62],
+        [12, 27, 53, 72, 89]
+    ]
