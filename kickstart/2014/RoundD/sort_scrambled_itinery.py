aeb5c60860004eec5247f2b06a5da18050d95724
satojkovic <satojkovic@gmail.com>
2022-09-19 22:17:19 +0900

    Update solution of kickstart 2014 RoundD problem
    Sort a scrambled itinerary.

--- kickstart/2014/RoundD/sort_scrambled_itinery.py
+++ kickstart/2014/RoundD/sort_scrambled_itinery.py
@@ -3,6 +3,7 @@ class GraphNode:
         self.val = val
         self.is_visited = False
         self.adj = None
+        self.dst_node = False
 
 
 class Graph:
@@ -15,6 +16,26 @@ class Graph:
         if not dst in self.nodes:
             self.nodes[dst] = GraphNode(dst)
         self.nodes[src].adj = self.nodes[dst]
+        self.nodes[dst].dst_node = True
+
+    def get_root(self):
+        root = None
+        for k in self.nodes.keys():
+            if not self.nodes[k].dst_node:
+                root = self.nodes[k]
+        return root
+
+
+def traverse(root):
+    path = []
+    while root:
+        path.append(root.val)
+        root = root.adj
+    return path
+
+
+def print_path(path):
+    return ' '.join([path[i] + '-' + path[i+1] for i in range(len(path) - 1)])
 
 
 def test_create_graph():
@@ -29,3 +50,24 @@ def test_create_graph():
         g.nodes['JFK'].adj == g.nodes['MIA'] and \
         g.nodes['MIA'].adj == g.nodes['ORD'] and \
         g.nodes['ORD'].adj == None
+
+    root = g.get_root()
+    assert root.val == 'SFO'
+
+
+def solve():
+    T = int(input())
+    for t in range(1, T + 1):
+        N = int(input())
+        flights = [[input().rstrip(), input().rstrip()] for _ in range(N)]
+        g = Graph()
+        for flight in flights:
+            src, dst = flight
+            g.add_edge(src, dst)
+        root = g.get_root()
+        path = traverse(root)
+        print('Case #{}: {}'.format(t, print_path(path)))
+
+
+if __name__ == '__main__':
+    solve()
