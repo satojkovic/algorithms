5d517beb33c1738a9a44464e011261afec19f8b2
satojkovic <satojkovic@gmail.com>
2022-10-05 21:59:16 +0900

    Update stack.py
    use array
    add test method

--- python/stack.py
+++ python/stack.py
@@ -4,6 +4,8 @@ class ListNode:
         self.next = None
 
 # Stack implemented by linked list
+
+
 class Stack:
     def __init__(self):
         self.top = None
@@ -37,28 +39,57 @@ class Stack:
             print(node.data)
             node = node.next
 
+
 class StackByList:
-    def __init__(self):
-        self.stack = []
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.top = 0
+        self.s = [0 for _ in range(self.capacity)]
 
     def is_empty(self):
-        return len(self.stack) == 0
+        return self.top == 0
+
+    def is_full(self):
+        return self.top == self.capacity
 
     def push(self, data):
-        self.stack.append(data)
+        if self.is_full():
+            return None
+        self.s[self.top] = data
+        self.top += 1
 
     def pop(self):
         if self.is_empty():
             return None
-        # Pop last: O(1)
-        return self.stack.pop()
+        ret = self.s[self.top - 1]
+        self.top -= 1
+        return ret
 
-if __name__ == "__main__":
-    stack = StackByList()
-    stack.push(1)
-    stack.push(2)
-    stack.push(3)
-    print(stack.pop())
-    print(stack.pop())
-    print(stack.pop())
-    print(stack.pop())
+    def size(self):
+        return self.top
+
+    def peek(self):
+        return self.s[self.top - 1] if not self.is_empty() else None
+
+
+def test_stack_by_list():
+    sl = StackByList(6)
+    sl.push(4)
+    sl.push(1)
+    sl.push(3)
+    sl.pop()
+    sl.push(8)
+    sl.pop()
+    assert sl.size() == 2
+    assert sl.peek() == 1
+
+    sl.pop()
+    sl.pop()
+    assert sl.size() == 0
+    assert sl.is_empty() is True
+    assert sl.pop() is None
+
+    [sl.push(i*10) for i in range(6)]
+    assert sl.size() == 6
+    assert sl.is_full() is True
+    assert sl.push(100) is None
