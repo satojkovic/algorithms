8e317fecbb20efe9e7022f3a38208dcbff0b33fe
satojkovic <satojkovic@gmail.com>
2022-10-12 23:34:04 +0900

    Add simple doubly linked list class

--- python/doubly_linked_list.py
+++ python/doubly_linked_list.py
@@ -8,6 +8,57 @@ class DoublyListNode:
         self.next = None
 
 
+class SimpleDoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def search(self, target):
+        # O(n)
+        x = self.head
+        while x and x.data != target:
+            x = x.next
+        return x
+
+    def insert(self, data):
+        # O(1)
+        node = DoublyListNode(data)
+        node.next = self.head
+        if self.head:
+            self.head.prev = node
+        self.head = node
+
+    def delete(self, target):
+        # Assume that the target node is already searched in a list.
+        # O(1)
+        if target.prev:
+            target.prev.next = target.next
+        else:
+            self.head = target.next
+        if target.next:
+            target.next.prev = target.prev
+
+
+def test_simple_doubly_linked_list():
+    simple_dl = SimpleDoublyLinkedList()
+    assert simple_dl.search(0) is None
+    simple_dl.insert(10)
+    simple_dl.insert(20)
+    simple_dl.insert(30)
+    assert simple_dl.head.data == 30
+
+    node = simple_dl.search(20)
+    assert node.data == 20
+    simple_dl.delete(node)
+    assert simple_dl.head.next.data == 10
+
+    node = simple_dl.search(10)
+    simple_dl.delete(node)
+    assert simple_dl.head.next is None
+    node = simple_dl.search(30)
+    simple_dl.delete(node)
+    assert simple_dl.head is None
+
+
 class DoublyLinkedList:
     def __init__(self):
         self.head = None
