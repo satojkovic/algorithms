2335dd91ce2d7cc131ea2e00fed0b2960815aebc
satojkovic <satojkovic@gmail.com>
2022-10-15 23:48:27 +0900

    Update test

--- python/linked_list.py
+++ python/linked_list.py
@@ -31,26 +31,6 @@ class SimpleLinkedList:
             target.next = target.next.next
 
 
-def test_simple_linked_list():
-    simple_ll = SimpleLinkedList()
-    assert simple_ll.search(10) is None
-
-    [simple_ll.insert(i) for i in range(10, 41, 10)]
-    assert simple_ll.head.data == 40
-
-    node = simple_ll.search(20)
-    simple_ll.delete(node)
-    assert simple_ll.head.next.next.data == 10
-
-    node = simple_ll.search(40)
-    simple_ll.delete(node)
-    assert simple_ll.head.data == 30
-
-    node = simple_ll.search(100)
-    simple_ll.delete(node)
-    assert simple_ll.head.data == 30
-
-
 def remove_nth_from_end(head, n):
     if head is None:
         return None
