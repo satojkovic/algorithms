f5ac83a810b0456ef010b3eb0226442a59995b9f
satojkovic <satojkovic@gmail.com>
2022-11-05 21:24:58 +0900

    Update binary_search

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,77 +1,26 @@
 #!/usr/bin/env python
 # -*- coding=utf-8 -*-
 
-def search_2d_mat(mat, target):
-    return binary_search_2d_mat(mat, target)
-
-def binary_search_2d_mat(mat, target):
-    ret = -1
-    for m in range(len(mat)):
-        ret = binary_search_r(mat[m], target, 0, len(mat[m]) - 1)
-        if ret != -1:
-            return True
-    return False
-
-def binary_search_r(data, target, left, right):
-    if left > right:
-        return -1
-    mid = (left + right) // 2
-    if data[mid] == target:
-        return mid
-    elif data[mid] > target:
-        return binary_search_r(data, target, left, mid - 1)
-    else:
-        return binary_search_r(data, target, mid + 1, right)
-
 def binary_search(data, target):
-    """binary search
+    def _binary_search(data, target, left, right):
+        if left > right:
+            return -1
 
-    Args:
-        data (list): input list (binary search works only on a sorted list)
-        target (int): target value
-    
-    Returns:
-        int: the index of the target value in the list
-    """
+        mid = left + (right - left) // 2
+        if data[mid] == target:
+            return mid
+        elif data[mid] > target:
+            return _binary_search(data, target, left, mid - 1)
+        else:
+            return _binary_search(data, target, mid + 1, right)
 
     return _binary_search(data, target, 0, len(data) - 1)
 
 
-def _binary_search(data, target, left, right):
-    """binary search algorithm
-
-    First, compare the target value with the element located at middle of the left and right bounds.
-    If the target value is greater than the value at middle, increase the left bound, else decrease
-    the right bound.
-    Repeat this process recursively until left bound is greater than right bound.
-    If the target value equal the value at middle, we return the index of middle.
-    If the target value is not present in the list, we return -1.
-    
-    Args:
-        data (list): input list
-        target (int): target value
-        left (int): left bound of the input list
-        right (int): right bound of the input list
-    
-    Returns:
-        int: the index of the target value in the list
-    """
-
-    if left > right:
-        return -1
-
-    mid = (left + right) // 2
-    if data[mid] == target:
-        return mid
-    elif data[mid] > target:
-        return _binary_search(data, target, left, mid - 1)
-    else:
-        return _binary_search(data, target, mid + 1, right)
-
 def binary_search_iter(data, target):
     left, right = 0, len(data) - 1
     while left <= right:
-        mid = (left + right) // 2
+        mid = left + (right - left) // 2
         if data[mid] == target:
             return mid
         elif target < data[mid]:
@@ -79,6 +28,7 @@ def binary_search_iter(data, target):
         else:
             left = mid + 1
     return -1
+
 
 def binary_search_sparse(strs, s, left, right):
     if left > right:
@@ -105,9 +55,18 @@ def binary_search_sparse(strs, s, left, right):
     else:
         return binary_search_sparse(strs, s, mid + 1, right)
 
+
 def test_binary_search():
     data = [1, 3, 4, 13, 40, 193]
     assert binary_search(data, 40) == 4
     assert binary_search(data, 11) == -1
     assert binary_search(data, 1) == 0
     assert binary_search(data, 193) == 5
+
+
+def test_binary_search_iter():
+    data = [1, 3, 4, 13, 40, 193]
+    assert binary_search_iter(data, 40) == 4
+    assert binary_search_iter(data, 11) == -1
+    assert binary_search_iter(data, 1) == 0
+    assert binary_search_iter(data, 193) == 5
--- python/test_binary_search.py
+++ python/test_binary_search.py
@@ -8,27 +8,17 @@ class TestBinarySearch(unittest.TestCase):
         self.data2 = [-1, 10, 29, 67, 998]
         self.data3 = [1, 1, 1]
 
-    def test_binary_search_r(self):
-        eq_(binary_search_r(self.data, 40, 0, len(self.data) - 1), 4)
-        eq_(binary_search_r(self.data, 1, 0, len(self.data) - 1), 0)
-        eq_(binary_search_r(self.data, 5, 0, len(self.data) - 1), -1)
-        eq_(binary_search_r(self.data, 76, 0, len(self.data) - 1), -1)
-        eq_(binary_search_r(self.data, 1000, 0, len(self.data) - 1), -1)
-        eq_(binary_search_r(self.data, 0, 0, len(self.data) - 1), -1)
+    def test_binary_search(self):
+        eq_(binary_search(self.data, 40), 4)
+        eq_(binary_search(self.data, 1), 0)
+        eq_(binary_search(self.data, 5), -1)
+        eq_(binary_search(self.data, 76), -1)
+        eq_(binary_search(self.data, 1000), -1)
+        eq_(binary_search(self.data, 0), -1)
 
-        eq_(binary_search_r(self.data3, 1, 0, len(self.data3) - 1), 1)
-        eq_(binary_search_r(self.data3, 2, 0, len(self.data3) - 1), -1)
+        eq_(binary_search(self.data3, 1), 1)
+        eq_(binary_search(self.data3, 2), -1)
 
-    def test_search_2d_mat(self):
-        mat = [
-            [1,   4,  7, 11, 15],
-            [2,   5,  8, 12, 19],
-            [3,   6,  9, 16, 22],
-            [10, 13, 14, 17, 24],
-            [18, 21, 23, 26, 30]
-        ]
-        eq_(search_2d_mat(mat, 5), True)
-        eq_(search_2d_mat(mat, 20), False)
 
 if __name__ == "__main__":
     unittest.main()
