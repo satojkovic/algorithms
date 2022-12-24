2d3674553f05ceb0047d8fc14b694c5bfc4b7daa
satojkovic <satojkovic@gmail.com>
2022-12-24 22:31:00 +0900

    Update and remove method

--- problems/two_sum.py
+++ problems/two_sum.py
@@ -22,21 +22,7 @@ def two_sum_brute_force(nums, target):
     return [-1, -1]
 
 
-def two_sum_two_pass(nums, target):
-    seen = {}
-    for i in range(len(nums)):
-        seen[nums[i]] = i
-    for i in range(len(nums)):
-        rest = target - nums[i]
-        if rest in seen and seen[rest] != i:
-            return [seen[rest], i]
-    return [-1, -1]
-
-
 def test_two_sum():
-    assert two_sum([1, 2, 7, 9], 9) == [1, 2]
-    assert two_sum([2, 2, 5, 4], 6) == [1, 3]
-    assert two_sum([1, 1, 1, 1, 1], 2) == [0, 1]
-    assert two_sum([3, 7, 11], 2) == [-1, -1]
+    assert two_sum([2, 7, 11, 15], 9) == [0, 1]
     assert two_sum([3, 2, 4], 6) == [1, 2]
-    assert two_sum([0], 10) == [-1, -1]
+    assert two_sum([3, 3], 6) == [0, 1]
