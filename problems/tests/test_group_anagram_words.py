f5370226206a8bc9d3e33f18033eb4d900013de3
satojkovic <satojkovic@gmail.com>
2022-12-29 18:09:01 +0900

    Update tests/test_group_anagram_words.py

--- problems/tests/test_group_anagram_words.py
+++ problems/tests/test_group_anagram_words.py
@@ -2,4 +2,7 @@ from src.group_anagram_words import *
 
 
 def test_group_anagram_words2():
-    assert group_anagram_words2(["eat","tea","tan","ate","nat","bat"]) == [["eat","tea","ate"],["tan","nat"],["bat"]]
+    ret = group_anagram_words2(["eat","tea","tan","ate","nat","bat"])
+    ans = [["eat","tea","ate"],["tan","nat"],["bat"]]
+    assert len(ret) == len(ans)
+    assert all([sorted(r) == sorted(a)] for r, a in zip(sorted(ret), sorted(ans)))
