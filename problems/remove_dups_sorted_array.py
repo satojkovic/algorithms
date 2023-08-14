1004c340822b2aa751b06eba7ca376b2414f1a66
satojkovic <satojkovic@gmail.com>
2023-08-14 13:51:00 +0900

    Refactoring: remove duplicates from sorted array problem

--- problems/remove_dups_sorted_array.py
+++ problems/remove_dups_sorted_array.py
@@ -6,22 +6,21 @@
 #
 # Algorithm:
 #  Use two pointers.
-#  When the condition nums[k] != nums[i] is True, non-duplicate value is found.
-#  Therefore you can insert its value to the position of index (k+1).
+#  When the condition nums[i] != seen is True, non-duplicate value is found.
+#  Therefore you can insert its value to the position of index last_insert_pos.
 def remove_dups(nums):
-    if len(nums) == 0:
-        return 0
-    k = 0
+    seen = nums[0]
+    last_insert_pos = 1
     for i in range(1, len(nums)):
-        if nums[k] != nums[i]:
-            k += 1
-            nums[k] = nums[i]
-    return k + 1
+        if nums[i] != seen:
+            nums[last_insert_pos] = nums[i]
+            last_insert_pos += 1
+            seen = nums[i]
+    return last_insert_pos
 
 
 def test_remove_dups():
     assert remove_dups([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]) == 5
-    assert remove_dups([]) == 0
     assert remove_dups([1, 10, 13]) == 3
     assert remove_dups([1, 1, 1, 1, 1]) == 1
     assert remove_dups([-1, -1, 0, 10, 11]) == 4
