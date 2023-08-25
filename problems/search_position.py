6caab78ee7fd2190791e965bdd9de64410536d52
satojkovic <satojkovic@gmail.com>
2023-08-25 23:13:19 +0900

    Fix binary search algorithm

--- problems/search_position.py
+++ problems/search_position.py
@@ -1,12 +1,14 @@
 def search_position(nums, target):
-    left, right = 0, len(nums)
-    while left < right:
+    left, right = -1, len(nums)
+    while right - left > 1:
         mid = left + (right - left) // 2
-        if nums[mid] >= target:
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
             right = mid
         else:
-            left = mid + 1
-    return left
+            left = mid
+    return right
 
 
 def test_search_position():
