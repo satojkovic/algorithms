ba0806ecbd4c3531b622e55d47809e3b8980047a
satojkovic <satojkovic@gmail.com>
2023-08-29 23:15:21 +0900

    Fix trie in longest common prefix problem

--- problems/longest_common_prefix.py
+++ problems/longest_common_prefix.py
@@ -1,28 +1,19 @@
 class TrieNode:
-    def __init__(self, val):
-        self.val = val
-        self.childs = {}
+    def __init__(self):
+        self.child = {}
         self.is_end = False
 
 
 def longest_common_prefix(strs):
-    if not strs:
-        return ''
     if len(strs) == 1:
         return strs[0]
 
     root = create_trie(strs[1:])
-    res = []
-    for c in strs[0]:
-        if not c in root.childs or len(root.childs.keys()) != 1 or root.is_end:
-            break
-        res.append(c)
-        root = root.childs[c]
-    return ''.join(res)
+    return search(root, strs[0])
 
 
 def create_trie(strs):
-    root = TrieNode(None)
+    root = TrieNode()
     for s in strs:
         root = insert(root, s)
     return root
@@ -31,18 +22,29 @@ def create_trie(strs):
 def insert(root, s):
     org_root = root
     for c in s:
-        if not c in root.childs:
-            root.childs[c] = TrieNode(c)
-        root = root.childs[c]
+        if c not in root.child:
+            root.child[c] = TrieNode()
+        root = root.child[c]
     root.is_end = True
     return org_root
 
 
+def search(root, s):
+    res = []
+    for c in s:
+        if c not in root.child or len(root.child) != 1 or root.is_end:
+            break
+        res.append(c)
+        root = root.child[c]
+    return "".join(res)
+
+
 def test_longest_common_prefix():
-    assert longest_common_prefix(['flower', 'flow', 'flight']) == 'fl'
-    assert longest_common_prefix(['', 'test']) == ''
-    assert longest_common_prefix(['abc']) == 'abc'
-    assert longest_common_prefix(['abc', 'bca', 'cab']) == ''
+    assert longest_common_prefix(["flower", "flow", "flight"]) == "fl"
+    assert longest_common_prefix(["", "test"]) == ""
+    assert longest_common_prefix(["test", ""]) == ""
+    assert longest_common_prefix(["abc"]) == "abc"
+    assert longest_common_prefix(["abc", "bca", "cab"]) == ""
 
 
 def longest_common_prefix2(strs):
