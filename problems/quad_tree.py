ff481ad19a10475a8b4a46d1467c4b57e69eee66
satojkovic <satojkovic@gmail.com>
2023-09-29 22:44:49 +0900

    Add new script

---
+++ problems/quad_tree.py
@@ -0,0 +1,78 @@
+import itertools
+
+
+class Node:
+    def __init__(self, val, isLeaf, topLeft, topRight, bottomLeft, bottomRight):
+        self.val = val
+        self.isLeaf = isLeaf
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.bottomLeft = bottomLeft
+        self.bottomRight = bottomRight
+
+
+def gen_quad_tree(grid):
+    def quad_tree(grid, top, left, bottom, right):
+        if all_same(grid, top, left, bottom, right):
+            return Node(grid[top][left], True, None, None, None, None)
+
+        # isLeaf is False
+        curr_root = Node(0, False, None, None, None, None)
+        med_row = (top + bottom) // 2
+        med_col = (left + right) // 2
+        curr_root.topLeft = quad_tree(grid, top, left, med_row, med_col)
+        curr_root.bottomLeft = quad_tree(grid, med_row, left, bottom, med_col)
+        curr_root.topRight = quad_tree(grid, top, med_col, med_row, right)
+        curr_root.bottomRight = quad_tree(grid, med_row, med_col, bottom, right)
+        return curr_root
+
+    return quad_tree(grid, 0, 0, len(grid), len(grid))
+
+
+def all_same(grid, top, left, bottom, right):
+    grid_sum = sum(
+        [
+            grid[row][col]
+            for row, col in itertools.product(range(top, bottom), range(left, right))
+        ]
+    )
+    n = bottom - top
+    return True if grid_sum == 0 or grid_sum == n**2 else False
+
+
+def test_gen_quad_tree():
+    import deepdiff
+
+    grid = [[0, 1], [1, 0]]
+    assert deepdiff.DeepDiff(
+        gen_quad_tree(grid), [[0, 1], [1, 0], [1, 1], [1, 1], [1, 0]]
+    )
+
+    grid = [
+        [1, 1, 1, 1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0, 0, 0, 0],
+    ]
+    assert deepdiff.DeepDiff(
+        gen_quad_tree(grid),
+        [
+            [0, 1],
+            [1, 1],
+            [0, 1],
+            [1, 1],
+            [1, 0],
+            None,
+            None,
+            None,
+            None,
+            [1, 0],
+            [1, 0],
+            [1, 1],
+            [1, 1],
+        ],
+    )
