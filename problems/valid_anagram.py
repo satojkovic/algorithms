7a6517ddaccc8cee2985f622355852ff83c87620
satojkovic <satojkovic@gmail.com>
2024-06-01 22:26:00 +0900

    Add new solution for valid anagram problem

--- problems/valid_anagram.py
+++ problems/valid_anagram.py
@@ -28,17 +28,51 @@ def valid_anagram2(s, t):
     return True if sum([v for _, v in chars.items()]) == 0 else False
 
 
+def valid_anagram3(s, t):
+    if len(s) != len(t):
+        return False
+
+    count_s, count_t = {}, {}
+    for i in range(len(s)):
+        count_s[s[i]] = 1 + count_s.get(s[i], 0)
+        count_t[t[i]] = 1 + count_t.get(t[i], 0)
+    for c in count_s:
+        if count_s[c] != count_t.get(c, 0):
+            return False
+    return True
+
+
+def valid_anagram4(s, t):
+    return sorted(s) == sorted(t)
+
+
 def test_valid_anagram():
-    assert valid_anagram('anagram', 'nagaram') is True
-    assert valid_anagram('rat', 'car') is False
-    assert valid_anagram('', 'abc') is False
-    assert valid_anagram('abc', '') is False
-    assert valid_anagram('', '') is True
+    assert valid_anagram("anagram", "nagaram") is True
+    assert valid_anagram("rat", "car") is False
+    assert valid_anagram("", "abc") is False
+    assert valid_anagram("abc", "") is False
+    assert valid_anagram("", "") is True
 
 
 def test_valid_anagram2():
-    assert valid_anagram2('anagram', 'nagaram') is True
-    assert valid_anagram2('rat', 'car') is False
-    assert valid_anagram2('', 'abc') is False
-    assert valid_anagram2('abc', '') is False
-    assert valid_anagram2('', '') is True
+    assert valid_anagram2("anagram", "nagaram") is True
+    assert valid_anagram2("rat", "car") is False
+    assert valid_anagram2("", "abc") is False
+    assert valid_anagram2("abc", "") is False
+    assert valid_anagram2("", "") is True
+
+
+def test_valid_anagram3():
+    assert valid_anagram2("anagram", "nagaram") is True
+    assert valid_anagram2("rat", "car") is False
+    assert valid_anagram2("", "abc") is False
+    assert valid_anagram2("abc", "") is False
+    assert valid_anagram2("", "") is True
+
+
+def test_valid_anagram4():
+    assert valid_anagram2("anagram", "nagaram") is True
+    assert valid_anagram2("rat", "car") is False
+    assert valid_anagram2("", "abc") is False
+    assert valid_anagram2("abc", "") is False
+    assert valid_anagram2("", "") is True
