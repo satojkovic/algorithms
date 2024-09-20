c1a57c805cd64e6b355d4035d7404e50c3f8ae8f
satojkovic <satojkovic@gmail.com>
2024-09-20 23:26:39 +0900

    Max subarray O(n^2) another solution

--- problems/max_subarray.py
+++ problems/max_subarray.py
@@ -21,8 +21,21 @@ def max_subarray_o2(data):
             maxsofar = max(maxsofar, sum)
     return maxsofar
 
+def max_subarray_o2_another(data):
+    maxsofar = 0
+    n = len(data)
+    cumsum = (n + 1) * [0]
+    for i in range(n):
+        cumsum[i + 1] = cumsum[i] + data[i]
+    for i in range(0, n):
+        for j in range(i, n):
+            sum = cumsum[j-1] - cumsum[i]
+            maxsofar = max(maxsofar, sum)
+    return maxsofar
+
 
 def test_max_subarray():
     data = [31, -41, 59, 26, -53, 58, 97, -93, -23, 84]
     assert max_subarray_o3(data) == 187
     assert max_subarray_o2(data) == 187
+    assert max_subarray_o2_another(data) == 187
