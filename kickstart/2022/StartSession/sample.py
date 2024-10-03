789866e2269820d02da55ef5eed6512c6e06cb37
satojkovic <satojkovic@gmail.com>
2024-10-03 22:55:32 +0900

    Use modulo

--- kickstart/2022/StartSession/sample.py
+++ kickstart/2022/StartSession/sample.py
@@ -2,6 +2,5 @@ T = int(input())
 for t in range(1, T + 1):
     N, M = list(map(int, input().split()))
     Cs = list(map(int, input().split()))
-    Cs_sum = sum(Cs)
-    remain = Cs_sum - M * (Cs_sum // M)
+    remain = sum(Cs) % M
     print('Case #{}: {}'.format(t, remain))
