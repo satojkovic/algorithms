714d804d5d6508ef9455e7dde24b9035503a4e0f
satojkovic <satojkovic@gmail.com>
2024-11-14 08:31:59 +0900

    Add bucketsort solution for top_k_freq

--- problems/top_k_freq.py
+++ problems/top_k_freq.py
@@ -34,11 +34,54 @@ def top_k_freq_heap(nums, k):
     return heapq.nlargest(k, freqs.keys(), key=freqs.get)
 
 
+def top_k_freq_heap2(nums, k):
+    import heapq
+    freqs = {}
+    for num in nums:
+        freqs[num] = freqs.get(num, 0) + 1
+    heap = []
+    for num, freq in freqs.items():
+        heapq.heappush(heap, (-freq, num))
+    res = []
+    while len(res) < k:
+        res.append(heapq.heappop(heap)[1])
+    return res
+
+
 def test_top_k_freq_heap():
     assert top_k_freq_heap([1, 1, 1, 2, 2, 3], 2) == [1, 2]
     assert top_k_freq_heap([1], 1) == [1]
     assert top_k_freq_heap([1, 2], 2) == [1, 2]
     assert top_k_freq_heap([3, 3, 1, 1, 10, 2, 10, 1], 3) == [1, 3, 10]
+
+
+def test_top_k_freq_heap2():
+    assert top_k_freq_heap2([1, 1, 1, 2, 2, 3], 2) == [1, 2]
+    assert top_k_freq_heap2([1], 1) == [1]
+    assert top_k_freq_heap2([1, 2], 2) == [1, 2]
+    assert top_k_freq_heap2([3, 3, 1, 1, 10, 2, 10, 1], 3) == [1, 3, 10]
+
+
+def top_k_freq_bucketsort(nums, k):
+    freqs = {}
+    for num in nums:
+        freqs[num] = freqs.get(num, 0) + 1
+    buckets = [[] for _ in range(len(nums) + 1)]
+    for num, freq in freqs.items():
+        buckets[freq].append(num)
+    res = []
+    for i in range(len(buckets) - 1, -1, -1):
+        for num in buckets[i]:
+            res.append(num)
+            if len(res) == k:
+                return res
+
+
+def test_top_k_freq_bucketsort():
+    assert top_k_freq_bucketsort([1, 1, 1, 2, 2, 3], 2) == [1, 2]
+    assert top_k_freq_bucketsort([1], 1) == [1]
+    assert top_k_freq_bucketsort([1, 2], 2) == [1, 2]
+    assert top_k_freq_bucketsort([3, 3, 1, 1, 10, 2, 10, 1], 3) == [1, 3, 10]
 
 
 if __name__ == '__main__':
