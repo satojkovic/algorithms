8daf125fbf08a393b41b253e683cfba3de81ca19
satojkovic <satojkovic@gmail.com>
2024-11-26 09:09:36 +0900

    Use sort tuple

--- problems/3sum.py
+++ problems/3sum.py
@@ -1,11 +1,13 @@
+def sort_tuple(tup):
+    return tuple(sorted(tup))
+
 def three_sum_bf(nums):
-    nums.sort()
     triplets = set()
     for i in range(len(nums)):
         for j in range(i+1, len(nums)):
             for k in range(j+1, len(nums)):
                 if sum([nums[i], nums[j], nums[k]]) == 0:
-                    triplets.add((nums[i], nums[j], nums[k]))
+                    triplets.add(sort_tuple((nums[i], nums[j], nums[k])))
     return list(triplets)
 
 
@@ -13,10 +15,6 @@ def test_three_sum_bf():
     assert three_sum_bf([-1,0,1,2,-1,-4]) == [(-1, 0, 1), (-1, -1, 2)]
     assert not three_sum_bf([0,1,1])
     assert three_sum_bf([0,0,0]) == [(0,0,0)]
-
-
-def sort_tuple(tup):
-    return tuple(sorted(tup))
 
 def update_triplets_by_two_sum(nums, target, triplets, target_idx):
     seen = set()
