5b8c32b6cf2d202f64d7623e334e49c4f98dbdf1
satojkovic <satojkovic@gmail.com>
2024-11-26 09:08:21 +0900

    Add other solution for 3sum

--- problems/3sum.py
+++ problems/3sum.py
@@ -13,3 +13,29 @@ def test_three_sum_bf():
     assert three_sum_bf([-1,0,1,2,-1,-4]) == [(-1, 0, 1), (-1, -1, 2)]
     assert not three_sum_bf([0,1,1])
     assert three_sum_bf([0,0,0]) == [(0,0,0)]
+
+
+def sort_tuple(tup):
+    return tuple(sorted(tup))
+
+def update_triplets_by_two_sum(nums, target, triplets, target_idx):
+    seen = set()
+    for i, num in enumerate(nums):
+        if i == target_idx:
+            continue
+        rest = target - num
+        if rest in seen:
+            triplets.add(sort_tuple((-target, rest, num)))
+        seen.add(num)
+    return triplets
+
+def three_sum_by_two_sum(nums):
+    triplets = set()
+    for i, num in enumerate(nums):
+        triplets = update_triplets_by_two_sum(nums, -num, triplets, i)
+    return list(triplets)
+
+def test_three_sum_by_two_sum():
+    assert three_sum_by_two_sum([-1,0,1,2,-1,-4]) == [(-1, 0, 1), (-1, -1, 2)]
+    assert not three_sum_by_two_sum([0,1,1])
+    assert three_sum_by_two_sum([0,0,0]) == [(0,0,0)]
