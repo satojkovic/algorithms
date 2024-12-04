4988bd6f21e7fb04e2d0a645f10f39a60e9e35e3
satojkovic <satojkovic@gmail.com>
2024-12-05 07:55:00 +0900

    Fix left_max and right_max calculation and total amount

--- problems/trap_water.py
+++ problems/trap_water.py
@@ -12,18 +12,19 @@ def trap_bf(height):
 
 
 def trap_bf2(height):
-    amount = 0
-    left_max = [height[0]] * len(height)
-    right_max = [height[-1]] * len(height)
+    total = 0
+    left_max = [0] * len(height)
+    right_max = [0] * len(height)
 
     for i in range(1, len(height)):
-        left_max[i] = max(left_max[i-1], height[i])
+        left_max[i] = max(left_max[i-1], height[i-1])
     for i in range(len(height) - 2, -1, -1):
-        right_max[i] = max(right_max[i+1], height[i])
+        right_max[i] = max(right_max[i+1], height[i+1])
 
     for i in range(1, len(height) - 1):
-        amount += min(left_max[i], right_max[i]) - height[i]
-    return amount
+        amount = min(left_max[i], right_max[i]) - height[i]
+        total = total + amount if amount >= 0 else total
+    return total
 
 
 def test_trap_bf():
