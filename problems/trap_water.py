1eb80fe1a0e76d670e11409d23946a9d8492f790
satojkovic <satojkovic@gmail.com>
2024-12-05 08:25:06 +0900

    Update with comments

--- problems/trap_water.py
+++ problems/trap_water.py
@@ -1,30 +1,33 @@
 def trap_bf(height):
-    amount = 0
+    total_amount = 0
+    # Calculate the amount of water that can be stored at each location
     for i in range(1, len(height) - 1):
         left_max = 0
         right_max = 0
+        # Update left_max and right_max by comparison including the value of the current position.
         for j in range(i, -1, -1):
             left_max = max(left_max, height[j])
         for j in range(i, len(height)):
             right_max = max(right_max, height[j])
-        amount += min(left_max, right_max) - height[i]
-    return amount
+        total_amount += min(left_max, right_max) - height[i]
+    return total_amount
 
 
-def trap_bf2(height):
-    total = 0
-    left_max = [0] * len(height)
-    right_max = [0] * len(height)
+def trap_bf_precompute(height):
+    total_amount = 0
+    left_max = [height[0]] * len(height)
+    right_max = [height[-1]] * len(height)
 
+    # left_max and right_max are pre-calculated
     for i in range(1, len(height)):
-        left_max[i] = max(left_max[i-1], height[i-1])
+        left_max[i] = max(left_max[i-1], height[i])
     for i in range(len(height) - 2, -1, -1):
-        right_max[i] = max(right_max[i+1], height[i+1])
+        right_max[i] = max(right_max[i+1], height[i])
 
     for i in range(1, len(height) - 1):
-        amount = min(left_max[i], right_max[i]) - height[i]
-        total = total + amount if amount >= 0 else total
-    return total
+        total_amount += min(left_max[i], right_max[i]) - height[i]
+    return total_amount
+
 
 
 def test_trap_bf():
@@ -36,10 +39,10 @@ def test_trap_bf():
     assert trap_bf([1, 1, 1, 1]) == 0
 
 
-def test_trap_bf2():
-    assert trap_bf2([0,1,0,2,1,0,1,3,2,1,2,1]) == 6
-    assert trap_bf2([4,2,0,3,2,5]) == 9
-    assert trap_bf2([1, 0, 2]) == 1
-    assert trap_bf2([4, 1, 2]) == 1
-    assert trap_bf2([1]) == 0
-    assert trap_bf2([1, 1, 1, 1]) == 0
+def test_trap_bf_precompute():
+    assert trap_bf_precompute([0,1,0,2,1,0,1,3,2,1,2,1]) == 6
+    assert trap_bf_precompute([4,2,0,3,2,5]) == 9
+    assert trap_bf_precompute([1, 0, 2]) == 1
+    assert trap_bf_precompute([4, 1, 2]) == 1
+    assert trap_bf_precompute([1]) == 0
+    assert trap_bf_precompute([1, 1, 1, 1]) == 0
