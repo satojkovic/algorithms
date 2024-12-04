1060bc45f9fa0d544be371a66a47ce38a4800e7c
satojkovic <satojkovic@gmail.com>
2024-12-05 08:30:16 +0900

    Add two pointers solution

--- problems/trap_water.py
+++ problems/trap_water.py
@@ -13,7 +13,7 @@ def trap_bf(height):
     return total_amount
 
 
-def trap_bf_precompute(height):
+def trap_precompute(height):
     total_amount = 0
     left_max = [height[0]] * len(height)
     right_max = [height[-1]] * len(height)
@@ -29,6 +29,22 @@ def trap_bf_precompute(height):
     return total_amount
 
 
+def trap_two_pointers(height):
+    total_amount = 0
+    left, right = 0, len(height) - 1
+    left_max, right_max = height[left], height[right]
+
+    while left < right:
+        if left_max < right_max:
+            left += 1
+            left_max = max(left_max, height[left])
+            total_amount += left_max - height[left]
+        else:
+            right -= 1
+            right_max = max(right_max, height[right])
+            total_amount += right_max - height[right]
+    return total_amount
+
 
 def test_trap_bf():
     assert trap_bf([0,1,0,2,1,0,1,3,2,1,2,1]) == 6
@@ -39,10 +55,19 @@ def test_trap_bf():
     assert trap_bf([1, 1, 1, 1]) == 0
 
 
-def test_trap_bf_precompute():
-    assert trap_bf_precompute([0,1,0,2,1,0,1,3,2,1,2,1]) == 6
-    assert trap_bf_precompute([4,2,0,3,2,5]) == 9
-    assert trap_bf_precompute([1, 0, 2]) == 1
-    assert trap_bf_precompute([4, 1, 2]) == 1
-    assert trap_bf_precompute([1]) == 0
-    assert trap_bf_precompute([1, 1, 1, 1]) == 0
+def test_trap_precompute():
+    assert trap_precompute([0,1,0,2,1,0,1,3,2,1,2,1]) == 6
+    assert trap_precompute([4,2,0,3,2,5]) == 9
+    assert trap_precompute([1, 0, 2]) == 1
+    assert trap_precompute([4, 1, 2]) == 1
+    assert trap_precompute([1]) == 0
+    assert trap_precompute([1, 1, 1, 1]) == 0
+
+
+def test_trap_two_pointers():
+    assert trap_two_pointers([0,1,0,2,1,0,1,3,2,1,2,1]) == 6
+    assert trap_two_pointers([4,2,0,3,2,5]) == 9
+    assert trap_two_pointers([1, 0, 2]) == 1
+    assert trap_two_pointers([4, 1, 2]) == 1
+    assert trap_two_pointers([1]) == 0
+    assert trap_two_pointers([1, 1, 1, 1]) == 0
