e01e27ce577cd42e56dfe412d1623592be03458c
satojkovic <satojkovic@gmail.com>
2024-12-22 15:35:12 +0900

    Add monotonic stack solution

--- problems/daily_temperatures.py
+++ problems/daily_temperatures.py
@@ -10,9 +10,29 @@ def daily_temperatures_bf(temperatures):
     return answer
 
 
+def daily_temperatures(temperatures):
+    res = [0] * len(temperatures)
+    stack = []
+
+    for i, t in enumerate(temperatures):
+        while stack and stack[-1][1] < t:
+            stack_i, stack_t = stack.pop()
+            res[stack_i] = i - stack_i
+        stack.append((i, t))
+
+    return res
+
 def test_daily_temperatures_bf():
     assert daily_temperatures_bf([73,74,75,71,69,72,76,73]) == [1,1,4,2,1,1,0,0]
     assert daily_temperatures_bf([30,40,50,60]) == [1,1,1,0]
     assert daily_temperatures_bf([30,60,90]) == [1, 1, 0]
     assert daily_temperatures_bf([10]) == [0]
     assert daily_temperatures_bf([20, 20, 20, 20, 20]) == [0, 0, 0, 0, 0]
+
+
+def test_daily_temperatures():
+    assert daily_temperatures([73,74,75,71,69,72,76,73]) == [1,1,4,2,1,1,0,0]
+    assert daily_temperatures([30,40,50,60]) == [1,1,1,0]
+    assert daily_temperatures([30,60,90]) == [1, 1, 0]
+    assert daily_temperatures([10]) == [0]
+    assert daily_temperatures([20, 20, 20, 20, 20]) == [0, 0, 0, 0, 0]
