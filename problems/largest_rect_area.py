f207c7d3f33c6908d915c01257f58c5982dd862e
satojkovic <satojkovic@gmail.com>
2024-12-25 09:00:15 +0900

    Add stack solution for largest rect area problem

--- problems/largest_rect_area.py
+++ problems/largest_rect_area.py
@@ -21,6 +21,21 @@ def largest_rect_area_bf2(heights):
     return res
 
 
+def largest_rect_area(heights):
+    res = 0
+    stack = []
+    for i, h in enumerate(heights):
+        start = i
+        while stack and stack[-1][1] > h:
+            index, height = stack.pop()
+            res = max(res, height * (i - index))
+            start = index
+        stack.append((start, h))
+    for index, height in stack:
+        res = max(res, height * (len(heights) - index))
+    return res
+
+
 def test_largest_rect_area_bf():
     assert largest_rect_area_bf([2,1,5,6,2,3]) == 10
     assert largest_rect_area_bf([1, 4]) == 4
@@ -35,3 +50,11 @@ def test_largest_rect_area_bf():
     assert largest_rect_area_bf2([1, 1, 1]) == 3
     assert largest_rect_area_bf2([8, 0, 1, 5]) == 8
     assert largest_rect_area_bf2([0, 7, 3, 4]) == 9
+
+def test_largest_rect_area():
+    assert largest_rect_area([2,1,5,6,2,3]) == 10
+    assert largest_rect_area([1, 4]) == 4
+    assert largest_rect_area([100]) == 100
+    assert largest_rect_area([1, 1, 1]) == 3
+    assert largest_rect_area([8, 0, 1, 5]) == 8
+    assert largest_rect_area([0, 7, 3, 4]) == 9
