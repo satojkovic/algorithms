5637a00587205a4748a926075c85fa3795f84520
satojkovic <satojkovic@gmail.com>
2025-02-12 19:05:39 +0900

    Find pivot and search in sorted array separately.

--- problems/search_rotated_sorted_array.py
+++ problems/search_rotated_sorted_array.py
@@ -34,3 +34,39 @@ def test_search():
     assert search([15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14], 14) == 11
     assert search([15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14], 23) == -1
     assert search([2, 2, 2, 3, 4, 2], 4) == 4
+
+
+
+def binary_search(nums, left, right, target):
+    while left <= right:
+        mid = left + (right - left) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+def search_in(nums, target):
+    left , right = 0, len(nums) - 1
+    while left <= right:
+        mid = left + (right - left) // 2
+        if nums[mid] > nums[-1]:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    res = binary_search(nums, 0, left - 1, target)
+    if res != -1:
+        return res
+
+    return binary_search(nums, left, len(nums) - 1, target)
+
+
+def test_search_in():
+    assert search_in([4,5,6,7,0,1,2], 0) == 4
+    assert search_in([4,5,6,7,0,1,2], 3) == -1
+    assert search_in([1], 0) == -1
+    assert search_in([1], 1) == 0
+    assert search_in([1,2,4,5,6,7,0], 0) == 6
