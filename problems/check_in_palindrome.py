48eed4c4bdad18fdc8346393387ba821d6e16495
satojkovic <satojkovic@gmail.com>
2025-05-02 20:44:57 +0900

    Rename variables

--- problems/check_in_palindrome.py
+++ problems/check_in_palindrome.py
@@ -2,23 +2,23 @@ def checkInclusion(s1: str, s2: str) -> bool:
     if len(s1) > len(s2):
         return False
 
-    s1_hash = {}
-    s2_hash = {}
+    s1_freq = {}
+    s2_freq = {}
 
     for i in range(len(s1)):
-        s1_hash[s1[i]] = 1 + s1_hash.get(s1[i], 0)
-        s2_hash[s2[i]] = 1 + s2_hash.get(s2[i], 0)
-    if s1_hash == s2_hash:
+        s1_freq[s1[i]] = 1 + s1_freq.get(s1[i], 0)
+        s2_freq[s2[i]] = 1 + s2_freq.get(s2[i], 0)
+    if s1_freq == s2_freq:
         return True
 
     left = 0
     for right in range(len(s1), len(s2)):
-        s2_hash[s2[right]] = 1 + s2_hash.get(s2[right], 0)
-        s2_hash[s2[left]] -= 1
-        if s2_hash[s2[left]] == 0:
-            del s2_hash[s2[left]]
+        s2_freq[s2[right]] = 1 + s2_freq.get(s2[right], 0)
+        s2_freq[s2[left]] -= 1
+        if s2_freq[s2[left]] == 0:
+            del s2_freq[s2[left]]
         left += 1
-        if s1_hash == s2_hash:
+        if s1_freq == s2_freq:
             return True
     return False
 
@@ -26,21 +26,21 @@ def checkInclusion2(s1: str, s2: str) -> bool:
     if len(s1) > len(s2):
         return False
 
-    s1_count = [0] * 26
-    s2_count = [0] * 26
+    s1_freq = [0] * 26
+    s2_freq = [0] * 26
 
     for i in range(len(s1)):
-        s1_count[ord(s1[i]) - ord('a')] += 1
-        s2_count[ord(s2[i]) - ord('a')] += 1
-    if s1_count == s2_count:
+        s1_freq[ord(s1[i]) - ord('a')] += 1
+        s2_freq[ord(s2[i]) - ord('a')] += 1
+    if s1_freq == s2_freq:
         return True
 
     left = 0
     for right in range(len(s1), len(s2)):
-        s2_count[ord(s2[right]) - ord('a')] += 1
-        s2_count[ord(s2[left]) - ord('a')] -= 1
+        s2_freq[ord(s2[right]) - ord('a')] += 1
+        s2_freq[ord(s2[left]) - ord('a')] -= 1
         left += 1
-        if s1_count == s2_count:
+        if s1_freq == s2_freq:
             return True
     return False
 
