a41a923fab4304f6ad740c364c15c2aff2cac068
satojkovic <satojkovic@gmail.com>
2025-05-13 19:48:14 +0900

    Add new problem and solution

---
+++ problems/max_sliding_window.py
@@ -0,0 +1,22 @@
+import heapq
+from typing import List
+
+def maxSlidingWindow(nums: List[int], k: int) -> List[int]:
+    heap = []
+    output = []
+    for i in range(len(nums)):
+        heapq.heappush(heap, (-nums[i], i))
+        if i >= k - 1:
+            while heap[0][1] <= i - k:
+                heapq.heappop(heap)
+            output.append(-heap[0][0])
+    return output
+
+
+def test_max_sliding_window():
+    assert maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3) == [3, 3, 5, 5, 6, 7]
+    assert maxSlidingWindow([1], 1) == [1]
+    assert maxSlidingWindow([1, -1], 1) == [1, -1]
+    assert maxSlidingWindow([9, 11, 0, 3, 5, 11], 6) == [11]
+    assert maxSlidingWindow([4, -2], 2) == [4]
+
