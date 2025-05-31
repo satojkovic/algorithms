5cedfff8b3923b0fd6b7657faf6dd6619322e605
satojkovic <satojkovic@gmail.com>
2025-05-31 16:32:54 +0900

    Add new solution with three steps

--- problems/reorder_list.py
+++ problems/reorder_list.py
@@ -20,6 +20,34 @@ def reorderList(head):
         right -= 1
     nodes[left].next = None
 
+def reorderList2(head):
+    """
+    Do not return anything, modify head in-place instead.
+    """
+    # Find the middle of the list
+    slow = head
+    fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+    # Reverse the latter half of the list
+    latter = slow.next
+    prev_node = slow.next = None
+    while latter:
+        node = latter.next
+        latter.next = prev_node
+        prev_node = latter
+        latter = node
+
+    # Merge the two lists
+    former = head
+    latter = prev_node
+    while latter:
+        f_node, l_node = former.next, latter.next
+        former.next, latter.next = latter, f_node
+        former, latter = f_node, l_node
+
 
 def test_reorder_list():
     # Create a linked list: 1 -> 2 -> 3 -> 4
@@ -60,6 +88,49 @@ def test_reorder_list():
     # Create a linked list: 1
     head = ListNode(1)
     reorderList(head)
+    # Check the order: 1
+    assert head.val == 1
+    assert head.next is None
+
+def test_reorder_list2():
+    # Create a linked list: 1 -> 2 -> 3 -> 4
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+
+    reorderList2(head)
+
+    # Check the order: 1 -> 4 -> 2 -> 3
+    assert head.val == 1
+    assert head.next.val == 4
+    assert head.next.next.val == 2
+    assert head.next.next.next.val == 3
+    assert head.next.next.next.next is None
+
+    # Create a linked list: 1 -> 2 -> 3
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    reorderList2(head)
+    # Check the order: 1 -> 3 -> 2
+    assert head.val == 1
+    assert head.next.val == 3
+    assert head.next.next.val == 2
+    assert head.next.next.next is None
+
+    # Create a linked list: 1 -> 2
+    head = ListNode(1)
+    head.next = ListNode(2)
+    reorderList2(head)
+    # Check the order: 1 -> 2
+    assert head.val == 1
+    assert head.next.val == 2
+    assert head.next.next is None
+
+    # Create a linked list: 1
+    head = ListNode(1)
+    reorderList2(head)
     # Check the order: 1
     assert head.val == 1
     assert head.next is None
