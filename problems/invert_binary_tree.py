01ea2b1605b384619537cc0d652a1b2abd42548d
satojkovic <satojkovic@gmail.com>
2025-09-07 22:34:07 +0900

    Step by step solution for inverting tree problem

--- problems/invert_binary_tree.py
+++ problems/invert_binary_tree.py
@@ -3,3 +3,11 @@ def invert_binary_tree(root):
         return None
     root.left, root.right = invert_binary_tree(root.right), invert_binary_tree(root.left)
     return root
+
+def invertTree(root):
+    if not root:
+        return None
+    inverted_left = invertTree(root.left)
+    inverted_right = invertTree(root.right)
+    root.left, root.right = inverted_right, inverted_left
+    return root
