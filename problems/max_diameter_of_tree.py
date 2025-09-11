aba3eaf07e9ac6f20ce1f509210309a8700e96a6
satojkovic <satojkovic@gmail.com>
2025-09-11 19:03:18 +0900

    Add solution and test for max diameter of tree problem

---
+++ problems/max_diameter_of_tree.py
@@ -0,0 +1,40 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        self.max_diameter = 0
+
+        def get_depth(root):
+            if not root:
+                return 0
+            left_depth = get_depth(root.left)
+            right_depth = get_depth(root.right)
+            self.max_diameter = max(left_depth + right_depth, self.max_diameter)
+            return max(left_depth, right_depth) + 1
+
+        get_depth(root)
+        return self.max_diameter
+
+
+def test_max_diameter_of_tree():
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
+
+    solution = Solution()
+    assert solution.diameterOfBinaryTree(root) == 3
+
+    root2 = TreeNode(1)
+    root2.left = TreeNode(2)
+
+    assert solution.diameterOfBinaryTree(root2) == 1
+
+    root3 = None
+    assert solution.diameterOfBinaryTree(root3) == 0
