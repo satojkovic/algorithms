d0c3eb13a6f0462690f008e8bced691795388252
satojkovic <satojkovic@gmail.com>
2025-09-25 22:41:43 +0900

    Update is_same_tree.py

--- problems/is_same_tree.py
+++ problems/is_same_tree.py
@@ -7,6 +7,9 @@ class TreeNode:
 def is_same_tree(p, q):
     if not p and not q:
         return True
-    if not p or not q or p.val != q.val:
+    if not p or not q:
+        return False
+
+    if p.val != q.val:
         return False
     return is_same_tree(p.left, q.left) and is_same_tree(p.right, q.right)
