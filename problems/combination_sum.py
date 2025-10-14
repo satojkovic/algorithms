a0036ceb8c21e2133d2909f375340a1181bd89a3
satojkovic <satojkovic@gmail.com>
2025-10-15 00:16:38 +0900

    Update combination sum

--- problems/combination_sum.py
+++ problems/combination_sum.py
@@ -1,23 +1,38 @@
-def combination_sum(k, n):
+def combination_sum(candidates, target):
+    def backtrack(remain, start_index, current_combination):
+        if remain == 0:
+            results.append(current_combination[:])
+            return
+        if remain < 0:
+            return
+
+        for i in range(start_index, len(candidates)):
+            current_combination.append(candidates[i])
+            backtrack(remain - candidates[i], i, current_combination)
+            current_combination.pop()
+
     results = []
-
-    def backtrack(remain, num_list, pivot):
-        if remain == 0 and len(num_list) == k:
-            results.append(list(num_list))
-            return
-        elif remain < 0 or len(num_list) == k:
-            return
-
-        for i in range(pivot, 10):
-            num_list.append(i)
-            backtrack(remain - i, num_list, i + 1)
-            num_list.pop()
-
-    backtrack(n, [], 1)
+    backtrack(target, 0, [])
     return results
 
 
 def test_combination_sum():
-    assert combination_sum(3, 7) == [[1, 2, 4]]
-    assert combination_sum(3, 9) == [[1, 2, 6], [1, 3, 5], [2, 3, 4]]
-    assert combination_sum(4, 1) == []
+    import deepdiff
+
+    assert not deepdiff.DeepDiff(
+        combination_sum([2, 3, 6, 7], 7), [[7], [2, 2, 3]], ignore_order=True
+    )
+    assert not deepdiff.DeepDiff(
+        combination_sum([2, 3, 5], 8),
+        [[2, 2, 2, 2], [2, 3, 3], [3, 5]],
+        ignore_order=True,
+    )
+    assert not deepdiff.DeepDiff(
+        combination_sum([2], 1), [], ignore_order=True
+    )
+    assert not deepdiff.DeepDiff(
+        combination_sum([1], 1), [[1]], ignore_order=True
+    )
+    assert not deepdiff.DeepDiff(
+        combination_sum([1], 2), [[1, 1]], ignore_order=True
+    )
