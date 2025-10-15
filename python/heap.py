cab5690c746207fcb297febecdef96d23c576d71
satojkovic <satojkovic@gmail.com>
2025-10-15 21:45:58 +0900

    Update MaxHeap and test method

--- python/heap.py
+++ python/heap.py
@@ -3,77 +3,51 @@
 
 class MaxHeap:
     def __init__(self):
-        self.data = []
+        self.heap = []
 
-    def heapify(self, data, p, size):
-        """Build a max heap
+    def _shift_up(self, idx):
+        while idx > 0:
+            parent = (idx - 1) // 2
+            if self.heap[parent] < self.heap[idx]:
+                self.heap[parent], self.heap[idx] = self.heap[idx], self.heap[parent]
+                idx = parent
+            else:
+                break
 
-        In a max heap, the parent node is always greater than or equal to its child nodes.
-        If p denotes the index of a parent node, then 2*p + 1 denotes the left child node and
-        2*p + 2 denotes the right child node.
-
-        A node with largest value among parent and child nodes will be a new parent node,
-        and building a max heap is repeated to a subtree of a child node swapped by previous parent node.
-
-        Args:
-            data (list): an input list
-            p (integer): a parent node index
-
-        Returns:
-            list: returns a max heap represented as one dimensional array
-        """
-        left_idx = 2 * p + 1
-        right_idx = 2 * p + 2
-        largest = p
-
-        if left_idx < size and data[largest] < data[left_idx]:
-            largest = left_idx
-        if right_idx < size and data[largest] < data[right_idx]:
-            largest = right_idx
-
-        if largest != p:
-            data[largest], data[p] = data[p], data[largest]
-            data = self.heapify(data, largest, size)
-
-        return data
-
-    def build(self, data):
-        tail = len(data) - 1
-        parent = (tail - 1) // 2
-        for p in range(parent, -1, -1):
-            data = self.heapify(data, p, size=len(data))
-        self.data = data
+    def _shift_down(self, idx):
+        data_size = len(self.heap)
+        while idx < data_size:
+            left_idx = 2 * idx + 1
+            right_idx = 2 * idx + 2
+            largest = idx
+            if left_idx < data_size and self.heap[largest] < self.heap[left_idx]:
+                largest = left_idx
+            if right_idx < data_size and self.heap[largest] < self.heap[right_idx]:
+                largest = right_idx
+            if largest == idx:
+                break
+            self.heap[largest], self.heap[idx] = self.heap[idx], self.heap[largest]
+            idx = largest
 
     def push(self, val):
-        self.data.append(val)
-        curr = len(self.data) - 1
-        while curr > 0:
-            parent = (curr - 1) // 2
-            if self.data[parent] >= self.data[curr]:
-                break
-            self.data[parent], self.data[curr] = self.data[curr], self.data[parent]
-            curr = parent
+        self.heap.append(val)
+        self._shift_up(len(self.heap) - 1)
 
     def pop(self):
-        if len(self.data) == 0:
+        if len(self.heap) == 0:
             return None
-        self.data[0], self.data[-1] = self.data[-1], self.data[0]
-        ret = self.data.pop()
-        curr = 0
-        data_size = len(self.data)
-        while curr < data_size:
-            left_idx = 2 * curr + 1
-            right_idx = 2 * curr + 2
-            largest = curr
-            if left_idx < data_size and self.data[largest] < self.data[left_idx]:
-                largest = left_idx
-            if right_idx < data_size and self.data[largest] < self.data[right_idx]:
-                largest = right_idx
-            if largest == curr:
-                break
-            self.data[largest], self.data[curr] = self.data[curr], self.data[largest]
-            curr = largest
+        self.heap[0], self.heap[-1] = self.heap[-1], self.heap[0]
+        ret = self.heap.pop()
+        self._shift_down(0)
         return ret
+
+    def peek(self):
+        if len(self.heap) == 0:
+            return None
+        return self.heap[0]
+
+    def __len__(self):
+        return len(self.heap)
 
 
 class MinHeap:
@@ -128,8 +102,17 @@ class MinHeap:
 # data from Algorithm Introduction 3rd edition
 def test_max_heap():
     max_heap = MaxHeap()
-    max_heap.build([4, 1, 3, 2, 16, 9, 10, 14, 8, 7])
-    assert max_heap.data == [16, 14, 10, 8, 7, 9, 3, 2, 4, 1]
+    values = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7]
+    for val in values:
+        max_heap.push(val)
+
+    # Internal representation is irrelevant
+    # If the results of the pop operation are in descending order, it is a valid max heap.
+    sorted_values = sorted(values, reverse=True)
+    popped_values = []
+    while len(max_heap) > 0:
+        popped_values.append(max_heap.pop())
+    assert popped_values == sorted_values
 
 
 def test_min_heap():
@@ -137,4 +120,11 @@ def test_min_heap():
     values = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7]
     for val in values:
         min_heap.push(val)
-    assert min_heap.heap == [1, 2, 3, 4, 7, 9, 10, 14, 8, 16]
+
+    # Internal representation is irrelevant
+    # If the results of the pop operation are in ascending order, it is a valid min
+    sorted_values = sorted(values)
+    popped_values = []
+    while len(min_heap) > 0:
+        popped_values.append(min_heap.pop())
+    assert popped_values == sorted_values
