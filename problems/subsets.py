66a0db6026e3e6221e3f8937cb4964f1f1f2697c
satojkovic <satojkovic@gmail.com>
2025-10-19 20:22:33 +0900

    Update subsets

--- problems/subsets.py
+++ problems/subsets.py
@@ -4,7 +4,7 @@
 #  (At each step, append the concatenated list to the result list)
 #  When this recursive search reach the last element, repeating the following step.
 #    Go back one step, replace the element with the next element of current index (until the last index)
-def subsets0(nums):
+def subsets(nums):
     def backtrack(start_index, curr_subset):
         res.append(curr_subset[:])
         for i in range(start_index, len(nums)):
@@ -17,55 +17,9 @@ def subsets0(nums):
     return res
 
 
-def subsets2(nums):
-    res = [[]]
-    for num in nums:
-        res += [r + [num] for r in res]
-    return res
 
-
-def subsets3(nums):
-    def add_subsets(curr, start):
-        if len(curr) == k:
-            res.append(curr[:])
-            return
-        for i in range(start, len(nums)):
-            curr.append(nums[i])
-            add_subsets(curr, i + 1)
-            curr.pop()
-
-    res = []
-    for k in range(len(nums) + 1):
-        add_subsets(curr=[], start=0)
-    return res
-
-
-def test_subsets0():
-    assert subsets0([1, 2, 3]) == [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]]
-    assert subsets0([1]) == [[], [1]]
-    assert subsets0([]) == [[]]
-    assert subsets0([5, 4]) == [[], [5], [5, 4], [4]]
-
-
-def test_subsets2():
-    import deepdiff
-
-    assert not deepdiff.DeepDiff(
-        subsets2([1, 2, 3]),
-        [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]],
-        ignore_order=True,
-    )
-    assert subsets2([1]) == [[], [1]]
-    assert subsets2([]) == [[]]
-
-
-def test_subsets3():
-    import deepdiff
-
-    assert not deepdiff.DeepDiff(
-        subsets3([1, 2, 3]),
-        [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]],
-        ignore_order=True,
-    )
-    assert subsets3([1]) == [[], [1]]
-    assert subsets3([]) == [[]]
+def test_subsets():
+    assert subsets([1, 2, 3]) == [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]]
+    assert subsets([1]) == [[], [1]]
+    assert subsets([]) == [[]]
+    assert subsets([5, 4]) == [[], [5], [5, 4], [4]]
