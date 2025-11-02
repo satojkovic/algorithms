ea0b70d2602a7021ce48deda36e7d2435125c7b1
satojkovic <satojkovic@gmail.com>
2025-11-02 15:07:58 +0900

    Add greedy solution

--- problems/jump_game.py
+++ problems/jump_game.py
@@ -24,7 +24,21 @@ def canJump_backtrack(self, nums: List[int]) -> bool:
     res = backtrack(0)
     return True if res == "Good" else False
 
+def canJump_greedy(nums: List[int]) -> bool:
+    reach = 0
+    for i in range(len(nums)):
+        if i > reach:
+            return False
+        reach = max(reach, i + nums[i])
+        if reach >= len(nums) - 1:
+            return True
+    return True
 
 def test_canJump_backtrack():
     assert canJump_backtrack(None, [2, 3, 1, 1, 4]) is True
     assert canJump_backtrack(None, [3, 2, 1, 0, 4]) is False
+
+
+def test_canJump_greedy():
+    assert canJump_greedy([2, 3, 1, 1, 4]) is True
+    assert canJump_greedy([3, 2, 1, 0, 4]) is False
