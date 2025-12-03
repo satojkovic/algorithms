b71c698122dbaf4a448f364efc0bf1cfd43fb3e8
satojkovic <satojkovic@gmail.com>
2025-12-03 23:55:41 +0900

    Refactoring

--- problems/delete_node.py
+++ problems/delete_node.py
@@ -1,19 +1,17 @@
-from linked_list import ListElement
-
 def delete_node(node):
-    if node.next_elem is None:
+    if node.next is None:
         node = None
     else:
         trav1 = node
-        trav2 = node.next_elem
+        trav2 = node.next
         while trav2 is not None:
             trav1.data = trav2.data
-            if trav2.next_elem is not None:
+            if trav2.next is not None:
                 trav1 = trav2
             else:
-                trav1.next_elem = None
-            trav2 = trav2.next_elem
+                trav1.next = None
+            trav2 = trav2.next
 
 def delete_node2(node):
-    node.data = node.next_elem.data
-    node.next_elem = node.next_elem.next_elem
+    node.data = node.next.data
+    node.next = node.next.next
