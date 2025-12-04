6357c0091700595133aecd8510901b9058036436
satojkovic <satojkovic@gmail.com>
2025-12-04 23:53:48 +0900

    Transpose and reverse

--- problems/rotate_image.py
+++ problems/rotate_image.py
@@ -8,6 +8,18 @@ def rotate(matrix):
             matrix[n - 1 - layer][n - 1 - i] = matrix[i][n - 1 - layer]
             matrix[i][n - 1 - layer] = top
 
+def rotate_eq_transpose_and_reverse(matrix):
+    n = len(matrix)
+    for row in range(n):
+        for col in range(row + 1, n):
+            matrix[row][col], matrix[col][row] = matrix[col][row], matrix[row][col]
+
+    for row in range(n):
+        for col in range(n // 2):
+            matrix[row][col], matrix[row][n - 1 - col] = matrix[row][n - 1- col], matrix[row][col]
+
+
+
 def test_rotate_image():
     matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     rotate(matrix)
@@ -21,6 +33,28 @@ def test_rotate_image():
         [21, 22, 23, 24, 25],
     ]
     rotate(matrix)
+    assert matrix == [
+        [21, 16, 11, 6, 1],
+        [22, 17, 12, 7, 2],
+        [23, 18, 13, 8, 3],
+        [24, 19, 14, 9, 4],
+        [25, 20, 15, 10, 5],
+    ]
+
+
+def test_rotate_eq_transpose_reverse():
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    rotate_eq_transpose_and_reverse(matrix)
+    assert matrix == [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+
+    matrix = [
+        [1, 2, 3, 4, 5],
+        [6, 7, 8, 9, 10],
+        [11, 12, 13, 14, 15],
+        [16, 17, 18, 19, 20],
+        [21, 22, 23, 24, 25],
+    ]
+    rotate_eq_transpose_and_reverse(matrix)
     assert matrix == [
         [21, 16, 11, 6, 1],
         [22, 17, 12, 7, 2],
