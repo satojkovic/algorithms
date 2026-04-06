ecffbf6b4e78204af2e96ea0e27f0a0a3a973889
satojkovic <satojkovic@gmail.com>
2026-04-06 20:14:25 +0900

    Add new script

---
+++ problems/dp/can_partition.py
@@ -0,0 +1,13 @@
+def canPartition(nums):
+    total_sum = sum(nums)
+    if total_sum % 2 != 0:
+        return False
+    target = total_sum // 2
+    dp = [False] * (target + 1)
+    dp[0] = True
+
+    for num in nums:
+        for s in range(target, num - 1, -1):
+            if dp[s - num]:
+                dp[s] = True
+    return dp[target]
